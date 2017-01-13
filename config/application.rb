6d7e4b7e4760a907d3210516bc21e01369061d50
Siobhan Jacobson <siobhan.jacobson@dartmouth.edu>
2017-01-13 10:50:03 -0500

    Rename app to Echo.

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module RailsTemplate
+module Echo
   class Application < Rails::Application
 
     config.time_zone = 'Eastern Time (US & Canada)'
