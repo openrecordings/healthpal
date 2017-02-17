02dad38aa047ee380f78e627a30def565d9b5b77
Rebecca Faill <rebecca@Rebeccas-MacBook-Pro-5.local>
2017-02-17 14:12:04 -0500

    Get rid of ECHO module for merge into base package

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Echo
+module RailsTemplate
   class Application < Rails::Application
 
     config.time_zone = 'Eastern Time (US & Canada)'
