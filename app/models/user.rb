4670e7324e681165adcb2ce913b97c1a02bcb8d1
Will Haslett <will.haslett@gmail.com>
2017-05-31 17:08:12 -0400

    Ran devise_otp migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :invitable, :database_authenticatable, :registerable,
+  devise :otp_authenticatable, :invitable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,24 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170111185343) do
+ActiveRecord::Schema.define(version: 20170531210643) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
     t.string   "invitation_token"
     t.datetime "invitation_created_at"
     t.datetime "invitation_sent_at"
@@ -37,10 +37,22 @@ ActiveRecord::Schema.define(version: 20170111185343) do
     t.integer  "invited_by_id"
     t.integer  "invitations_count",      default: 0
     t.string   "role"
+    t.string   "otp_auth_secret"
+    t.string   "otp_recovery_secret"
+    t.boolean  "otp_enabled",            default: false, null: false
+    t.boolean  "otp_mandatory",          default: false, null: false
+    t.datetime "otp_enabled_on"
+    t.integer  "otp_failed_attempts",    default: 0,     null: false
+    t.integer  "otp_recovery_counter",   default: 0,     null: false
+    t.string   "otp_persistence_seed"
+    t.string   "otp_session_challenge"
+    t.datetime "otp_challenge_expires"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["invitation_token"], name: "index_users_on_invitation_token", unique: true, using: :btree
     t.index ["invitations_count"], name: "index_users_on_invitations_count", using: :btree
     t.index ["invited_by_id"], name: "index_users_on_invited_by_id", using: :btree
+    t.index ["otp_challenge_expires"], name: "index_users_on_otp_challenge_expires", using: :btree
+    t.index ["otp_session_challenge"], name: "index_users_on_otp_session_challenge", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
