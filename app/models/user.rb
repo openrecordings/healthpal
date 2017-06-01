6f33a0c47c970345fcfbe54556eb2d40b81b44a4
Will Haslett <will.haslett@gmail.com>
2017-06-01 09:42:50 -0400

    Updated user model with otp methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,42 @@ class User < ApplicationRecord
     role == root
   end
 
+  # Resets otp so that a new device is registered upon next login
+  def deprovision_otp
+    set_otp_credentials
+    self.otp_enabled = false
+    self.otp_enabled_on = nil
+    save!
+  end
+
+  # Disables otp
+  def disable_otp
+    deprovision_otp
+    self.update(otp_mandatory: false)
+  end
+
+  # Enables otp
+  def enable_otp
+    set_otp_credentials
+    save!
+  end
+
+  # Disables or enables 2FA based on current state
+  def toggle_otp
+    if otp_mandatory
+      disable_otp
+    else
+      enable_otp
+    end
+  end
+
+  private
+
+  def set_otp_credentials
+    self.otp_auth_secret = ROTP::Base32.random_base32
+    self.otp_recovery_secret = ROTP::Base32.random_base32
+    self.otp_persistence_seed = SecureRandom.hex
+    self.otp_mandatory = true
+  end
+
 end
