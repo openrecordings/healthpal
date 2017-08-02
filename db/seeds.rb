b6b9b0555d3155b189a79b1e63b043b07ffb7cc9
Will Haslett <will.haslett@gmail.com>
2017-08-02 12:49:02 -0400

    Disabled otp for root by default

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,11 @@
 # Create root user
 unless User.where(email: Rails.application.config.root_email).first
   puts 'Creating root user'
-  User.new({
+  user = User.new({
     email: Rails.application.config.root_email,
     password: Rails.application.config.root_password,
     role: 'root'
-  }).save
+  })
+  user.save!
+  user.disable_otp
 end
