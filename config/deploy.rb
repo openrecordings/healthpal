1c525da346eb11b907b42ec9cd5faccdc2e90eff
Will Haslett <will.haslett@gmail.com>
2017-08-02 12:47:23 -0400

    Added recordings_tmp folder to shared folders on deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ lock ">=3.8.0"
 set :application, "orals"
 set :repo_url, "git@bitbucket.org:dartmouthinformatics/orals.git"
 set :linked_files, %w{config/database.yml config/application.yml config/secrets.yml }
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system recordings_tmp}
 set :deploy_to, '/home/rails/orals'
 set :branch, 'master'
 set :branch, ENV['BRANCH'] if ENV['BRANCH']
