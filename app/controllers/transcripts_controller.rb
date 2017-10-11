abc22051c877574eec3129933d93f08b4df9c32e
Will Haslett <will.haslett@gmail.com>
2017-10-11 16:23:38 -0400

    Working on Transcripts#create

--- app/controllers/transcripts_controller.rb
+++ app/controllers/transcripts_controller.rb
@@ -1,9 +1,27 @@
 class TranscriptsController < ApplicationController
 
+  def index
+  end
+
   def new
+    @transcript = Transcript.new format: :acusis
   end
 
   def create
+    file_to_text
+    @transcript = Transcript.new(transcript_params)
+    # TODO: convert raw to actual tempfile contents
+    @transcript.raw_to_ascii 
+    puts '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
+    puts transcript_params.inspect
+    puts '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
+    render :new
+  end
+
+  private
+
+  def transcript_params
+    params.require(:transcript).permit(:format, :recording_id, :raw)
   end
 
 end
