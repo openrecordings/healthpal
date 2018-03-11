0676fcf34e9f85c993da80d622d22da636567ee2
Will Haslett <will.haslett@gmail.com>
2018-03-11 15:13:38 -0400

    Working on setting duration upon save

--- app/models/recording.rb
+++ app/models/recording.rb
@@ -1,7 +1,32 @@
 class Recording < ApplicationRecord
+
   belongs_to :user
-  has_one :transcript
+  has_one :transcript, dependent: :destroy
   has_many :tags, through: :transcript
 
   attr_encrypted :audio, key: Rails.application.config.audio_encryption_key, encode: false, encode_iv: false
+
+  after_save :set_duration
+  TMP_PATH = "#{Rails.root}/recordings_tmp"
+
+  def path
+    "#{TMP_PATH}/#{self.id}.ogg"
+  end
+
+  # Creates tmp file for recording and returns file size in bytes
+  def make_tmp_file!
+    File.open(path, 'wb') { |file| file.write(self.audio) }
+  end
+
+  def tmp_file_size
+    File.exists?(path) ? File.size(path) : nil
+  end
+
+  private
+
+  def set_duration
+    make_tmp_file!
+    self.update! duration: (tmp_file_size / 10332)
+  end
+
 end
