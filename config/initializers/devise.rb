288882a55136c1c627226d875f2a9aa51364e358
Will Haslett <will.haslett@gmail.com>
2018-04-16 13:14:07 -0400

    Regular users now disabled after first session

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -309,6 +309,11 @@ Devise.setup do |config|
   #   manager.default_strategies(scope: :user).unshift :some_external_strategy
   # end
 
+  # Auto-disable regular user accounts upon first logoff
+  Warden::Manager.before_logout do |user, auth, opts|
+    user.disable_after_first_session!
+  end
+
   # ==> Mountable engine configurations
   # When using Devise inside an engine, let's call it `MyEngine`, and this engine
   # is mountable, there are some extra configurations to be taken into account.
