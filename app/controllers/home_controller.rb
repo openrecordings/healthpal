7be641a87b3c13e7e4e9209d4c05a93f79790aca
Will Haslett <will.haslett@gmail.com>
2018-10-26 16:25:48 -0400

    Close with the new duration calc, but the number is wrong :)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
 
   def index
     # TODO: Temporarily starting on recordings page until we have a real landing page
-    redirect_to :my_recordings
+    redirect_to (current_user&.privileged? ? :recordings : :my_recordings)
   end
 
 end
--- app/models/recording.rb
+++ app/models/recording.rb
@@ -15,9 +15,14 @@ class Recording < ApplicationRecord
   before_create :set_duration
 
   TMP_PATH = "#{Rails.root}/recordings_tmp"
+  DURATION_RGX = /\d\d:\d\d:\d\d.\d\d/
 
-  def path
+  def ogg_file
     "#{TMP_PATH}/#{self.id}.ogg"
+  end
+
+  def wav_file
+    "#{TMP_PATH}/#{self.id}.wav"
   end
 
   def tmp_file_size
@@ -27,8 +32,13 @@ class Recording < ApplicationRecord
   private
 
   def set_duration
-    File.open(path, 'wb') { |file| file.write(self.audio) } unless File.exists? path
-    self.duration = 4000
+    File.open(ogg_file, 'wb') { |file| file.write(self.audio) } unless File.exists? ogg_file
+    `ffmpeg -i #{ogg_file} -ar 100 -y #{wav_file}`
+    duration_row = `ffmpeg -i #{wav_file} 2>&1 | grep Duration`
+    wav_duration = duration_row.match(DURATION_RGX).to_s
+    minutes = (wav_duration[6] + wav_duration[7]).to_i
+    seconds = (wav_duration[9] + wav_duration[1]).to_i
+    self.duration = minutes * 60 + seconds
   end
 
 end
