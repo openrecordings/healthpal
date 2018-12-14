4315d4ce3d25312cadd9fe10e0a73d884dd6af34
Will Haslett <will.haslett@gmail.com>
2018-12-14 11:57:34 -0500

    Tabs are broken. Gem is ancient. Switching to the one people are using now.

--- app/assets/javascripts/recordings.js
+++ app/assets/javascripts/recordings.js
@@ -1,19 +1,19 @@
 // Set up tabs on pages with a #tabs element
-$(document).ready(function() {
-    $(".tabs").tabs();
-    console.log('here');
-    // Register editable recording columns
-    ['provider', 'patient_identifier'].forEach(function(c){
-        var field = $('.edit_' + c);
-        if(field.length > 0) {
-            field.editable({
-                type: 'text',
-                name: c,
-                mode: 'inline',
-                inputclass: 'editable-input',
-                value: '',
-                placeholder: $(field[0]).text() // All of the same kind of field have same placeholders
-            });
-        }
-    });
-});
+// $(document).ready(function() {
+//     $(".tabs").tabs();
+//     console.log('here');
+//     // Register editable recording columns
+//     ['provider', 'patient_identifier'].forEach(function(c){
+//         var field = $('.edit_' + c);
+//         if(field.length > 0) {
+//             field.editable({
+//                 type: 'text',
+//                 name: c,
+//                 mode: 'inline',
+//                 inputclass: 'editable-input',
+//                 value: '',
+//                 placeholder: $(field[0]).text() // All of the same kind of field have same placeholders
+//             });
+//         }
+//     });
+// });
