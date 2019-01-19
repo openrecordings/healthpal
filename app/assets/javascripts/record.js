5b44b0c176716abaa67e792b845d1747525e7eec
Will Haslett <will.haslett@gmail.com>
2019-01-19 14:31:23 -0500

    Starting video capture works

--- app/assets/javascripts/record.js
+++ app/assets/javascripts/record.js
@@ -1,22 +1,26 @@
 $(document).ready(function() {
+  
+  // Check for getUserMedia browser support
 	function hasGetUserMedia() {
 		return !!(navigator.mediaDevices &&
 			navigator.mediaDevices.getUserMedia);
 	}
-
 	if (hasGetUserMedia()) {
 	  console.log('getUserMedia found');	
 	} else {
+    //TODO: Alert the user
 	  console.log('getUserMedia not found');	
 	}
+
 });
 
-// For video recording:
-//const constraints = {
-//  video: true
-//};
-//
-//const video = document.querySelector('video');
-//
-//navigator.mediaDevices.getUserMedia(constraints).
-//  then((stream) => {video.srcObject = stream});
+$('#record-button').click(function(){
+  const constraints = {
+    video: true
+  };
+
+  const video = document.querySelector('video');
+
+  navigator.mediaDevices.getUserMedia(constraints).
+    then((stream) => {video.srcObject = stream});
+})
