96c9cf45649133020c38f3e9708dd67671bac5c0
Will Haslett <will.haslett@gmail.com>
2019-01-19 15:16:07 -0500

    Selecting media sources may be working. Not tested.

--- app/assets/javascripts/record.js
+++ app/assets/javascripts/record.js
@@ -14,13 +14,64 @@ $(document).ready(function() {
 
 });
 
-$('#record-button').click(function(){
+// From https://www.html5rocks.com/en/tutorials/getusermedia/intro/
+const videoElement = document.querySelector('video');
+const audioSelect = document.querySelector('select#audioSource');
+const videoSelect = document.querySelector('select#videoSource');
+
+navigator.mediaDevices.enumerateDevices()
+  .then(gotDevices).then(getStream).catch(handleError);
+
+audioSelect.onchange = getStream;
+videoSelect.onchange = getStream;
+
+function gotDevices(deviceInfos) {
+
+	console.log(deviceInfos);
+
+  for (let i = 0; i !== deviceInfos.length; ++i) {
+    const deviceInfo = deviceInfos[i];
+    const option = document.createElement('option');
+    option.value = deviceInfo.deviceId;
+    if (deviceInfo.kind === 'audioinput') {
+      option.text = deviceInfo.label ||
+        'microphone ' + (audioSelect.length + 1);
+      audioSelect.appendChild(option);
+    } else if (deviceInfo.kind === 'videoinput') {
+      option.text = deviceInfo.label || 'camera ' +
+        (videoSelect.length + 1);
+      videoSelect.appendChild(option);
+    } else {
+      console.log('Found another kind of device: ', deviceInfo);
+    }
+  }
+}
+
+function getStream() {
+  if (window.stream) {
+    window.stream.getTracks().forEach(function(track) {
+      track.stop();
+    });
+  }
+
   const constraints = {
-    video: true
+    audio: {
+      deviceId: {exact: audioSelect.value}
+    },
+    video: {
+      deviceId: {exact: videoSelect.value}
+    }
   };
 
-  const video = document.querySelector('video');
-
   navigator.mediaDevices.getUserMedia(constraints).
-    then((stream) => {video.srcObject = stream});
-})
+    then(gotStream).catch(handleError);
+}
+
+function gotStream(stream) {
+  window.stream = stream; // make stream available to console
+  videoElement.srcObject = stream;
+}
+
+function handleError(error) {
+  console.error('Error: ', error);
+}
