0d484ae15281459e9270de933e4bc72eb5e16cf3
Will Haslett <will.haslett@gmail.com>
2019-01-22 08:52:38 -0500

    Adapter.js is being loaded, but still getting getAudioTracks/mediaStream error

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,5 +7,5 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
+import adapter from 'webrtc-adapter';
 console.log('Hello World from Webpacker')
-require('webrtc-adapter');
