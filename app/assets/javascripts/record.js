d88fcd02af1c77ae5dea740166682213bbb4c6fe
Will Haslett <will.haslett@gmail.com>
2019-01-22 07:44:26 -0500

    Webpacker loads. Still need adapter.js

--- app/assets/javascripts/record.js
+++ app/assets/javascripts/record.js
@@ -4,124 +4,127 @@ const videoSelect = document.querySelector('select#videoSource');
 var streamRecorder;
 var recordStream;
 
-// Stream management. Started with https://www.html5rocks.com/en/tutorials/getusermedia/intro/ 
-//////////////////////////////////////////////////////////////////////////////////////////////////
-audioSelect.onchange = getStream;
-videoSelect.onchange = getStream;
+// Wrap entire file in conditional, verifying that we are on the recording page
+if(videoElement && audioSelect &&  videoSelect) {
 
-function gotDevices(deviceInfos) {
-  for (let i = 0; i !== deviceInfos.length; ++i) {
-    const deviceInfo = deviceInfos[i];
-    const option = document.createElement('option');
-    option.value = deviceInfo.deviceId;
-    if (deviceInfo.kind === 'audioinput') {
-      option.text = deviceInfo.label ||
-        'microphone ' + (audioSelect.length + 1);
-      audioSelect.appendChild(option);
-    } else if (deviceInfo.kind === 'videoinput') {
-      option.text = deviceInfo.label || 'camera ' +
-        (videoSelect.length + 1);
-      videoSelect.appendChild(option);
+  // Stream management. Started with https://www.html5rocks.com/en/tutorials/getusermedia/intro/ 
+  //////////////////////////////////////////////////////////////////////////////////////////////////
+  audioSelect.onchange = getStream;
+  videoSelect.onchange = getStream;
+
+  function gotDevices(deviceInfos) {
+    for (let i = 0; i !== deviceInfos.length; ++i) {
+      const deviceInfo = deviceInfos[i];
+      const option = document.createElement('option');
+      option.value = deviceInfo.deviceId;
+      if (deviceInfo.kind === 'audioinput') {
+        option.text = deviceInfo.label ||
+          'microphone ' + (audioSelect.length + 1);
+        audioSelect.appendChild(option);
+      } else if (deviceInfo.kind === 'videoinput') {
+        option.text = deviceInfo.label || 'camera ' +
+          (videoSelect.length + 1);
+        videoSelect.appendChild(option);
+      } else {
+        console.log('Found another kind of device: ', deviceInfo);
+      }
+    }
+  }
+
+  function handleError(error) {
+    console.error('Error: ', error);
+  }
+    
+  // Check for getUserMedia browser support
+  function hasGetUserMedia() {
+    return !!(navigator.mediaDevices &&
+      navigator.mediaDevices.getUserMedia);
+  }
+
+  function gotStream(stream) {
+    console.log('got');
+    // Make stream available to console
+    window.stream = stream;
+    recordStream = stream;
+  }
+
+  function stopStream() {
+    console.log('stop');
+    if (window.stream) {
+      window.stream.getTracks().forEach(function(track) {
+        track.stop();
+      });
+    }
+  }
+
+  function getStream() {
+    console.log('get');
+    const constraints = {
+      audio: {
+        deviceId: {exact: audioSelect.value}
+      },
+      video: {
+        deviceId: {exact: videoSelect.value}
+      }
+    };
+    navigator.mediaDevices.getUserMedia(constraints).
+      then(gotStream).catch(handleError);
+  }
+
+  // Recording. Started with https://stackoverflow.com/a/16784618
+  //////////////////////////////////////////////////////////////////////////////////////////////////
+  function onVideoFail(e) {
+    console.log('webcam fail!', e);
+  };
+
+  function startRecording() {
+    var mediaRecorder = new MediaRecorder(recordStream);
+    mediaRecorder.mimeType = 'video/webm';
+    mediaRecorder.ondataavailable = function(blob) {
+      videoElement.srcObject = recordStream;
+      postMediaToServer(videoElement.srcObject);
+    };
+    mediaRecorder.start(3000)
+    setTimeout(stopRecording, 10000);
+  }
+
+  function stopRecording() {
+    // TODO
+  }
+
+  function postMediaToServer(mediaBlob) {
+    var data = {};
+    data.video = mediaBlob;
+    data.metadata = 'test metadata';
+    data.action = "upload_video";
+    jQuery.post('/upload', data, onUploadSuccess);
+  }
+
+  function onUploadSuccess() {
+    console.log('video uploaded');
+  }
+
+  // onload
+  //////////////////////////////////////////////////////////////////////////////////////////////////
+  $(document).ready(function() {
+    if (hasGetUserMedia()) {
+      navigator.mediaDevices.enumerateDevices().then(gotDevices).catch(handleError);
+
+      $('#media-start-button').click(function(){
+        getStream();
+      })
+
+      $('#record-start-button').click(function(){
+        startRecording();
+      })
+
+      $('#record-stop-button').click(function(){
+        stopStream();
+        stopRecording();
+      })
     } else {
-      console.log('Found another kind of device: ', deviceInfo);
+      //TODO: Alert the user
+      console.log('getUserMedia not found');  
     }
-  }
+  });
 }
-
-function handleError(error) {
-  console.error('Error: ', error);
-}
-  
-// Check for getUserMedia browser support
-function hasGetUserMedia() {
-  return !!(navigator.mediaDevices &&
-    navigator.mediaDevices.getUserMedia);
-}
-
-function gotStream(stream) {
-  console.log('got');
-  // Make stream available to console
-  window.stream = stream;
-  recordStream = stream;
-}
-
-function stopStream() {
-  console.log('stop');
-  if (window.stream) {
-    window.stream.getTracks().forEach(function(track) {
-      track.stop();
-    });
-  }
-}
-
-function getStream() {
-  console.log('get');
-  const constraints = {
-    audio: {
-      deviceId: {exact: audioSelect.value}
-    },
-    video: {
-      deviceId: {exact: videoSelect.value}
-    }
-  };
-  navigator.mediaDevices.getUserMedia(constraints).
-    then(gotStream).catch(handleError);
-}
-
-// Recording. Started with https://stackoverflow.com/a/16784618
-//////////////////////////////////////////////////////////////////////////////////////////////////
-function onVideoFail(e) {
-  console.log('webcam fail!', e);
-};
-
-function startRecording() {
-  var mediaRecorder = new MediaRecorder(recordStream);
-  mediaRecorder.mimeType = 'video/webm';
-  mediaRecorder.ondataavailable = function(blob) {
-    videoElement.srcObject = recordStream;
-    postMediaToServer(videoElement.srcObject);
-  };
-  mediaRecorder.start(3000)
-  setTimeout(stopRecording, 10000);
-}
-
-function stopRecording() {
-  // TODO
-}
-
-function postMediaToServer(mediaBlob) {
-  var data = {};
-  data.video = mediaBlob;
-  data.metadata = 'test metadata';
-  data.action = "upload_video";
-  jQuery.post('/upload', data, onUploadSuccess);
-}
-
-function onUploadSuccess() {
-  console.log('video uploaded');
-}
-
-// onload
-//////////////////////////////////////////////////////////////////////////////////////////////////
-$(document).ready(function() {
-  if (hasGetUserMedia()) {
-    navigator.mediaDevices.enumerateDevices().then(gotDevices).catch(handleError);
-
-    $('#media-start-button').click(function(){
-      getStream();
-    })
-
-    $('#record-start-button').click(function(){
-      startRecording();
-    })
-
-    $('#record-stop-button').click(function(){
-      stopStream();
-      stopRecording();
-    })
-  } else {
-    //TODO: Alert the user
-    console.log('getUserMedia not found');  
-  }
-
-});
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,3 +8,4 @@
 // layout file, like app/views/layouts/application.html.erb
 
 console.log('Hello World from Webpacker')
+require('webrtc-adapter');
