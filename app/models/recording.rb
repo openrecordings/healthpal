575706cc3200afbe0ddfe8605b66b60945c521a2
Will Haslett <will.haslett@gmail.com>
2019-01-24 13:05:42 -0500

    Gutted Recording#transcribe again. Switching to HTTP calls instead of gcloud client

--- app/models/recording.rb
+++ app/models/recording.rb
@@ -18,23 +18,6 @@ class Recording < ApplicationRecord
   before_create :set_duration
 
   def transcribe
-    # NOTE: Currently only supports Google STT
-    transcript = Transcript.new(recording: self, source: :google)
-    stt_cmd = TTY::Command.new
-    stt_file = 'gs://health-pal-bucket/ge10.flac' 
-    stt_string = "gcloud ml speech recognize-long-running #{stt_file} --language-code='en-US' --async --include-word-time-offsets --encoding='flac'"
-    # Start the STT job
-    stt_cmd_stdout, stt_cmd_stderr = stt_cmd.run(stt_string)
-    Rails.logger.debug(stt_cmd_stderr) and return if stt_cmd_stderr
-    stt_job_name = JSON.parse(stt_cmd_stdout).name
-    poll_cmd = TTY::Command.new
-    poll_String = "gcloud ml speech operations wait #{stt_job_name}"
-    # Poll the STT job and get transcript JSON (stdout)
-    poll_cmd_stdout, pol_cmd_stderr = stt_cmd.run(poll_string)
-    Rails.logger.debug(poll_cmd_stderr) and return if poll_cmd_stderr
-    transcript.json = JSON.parse(poll_cmd_stdout)
-    transcript.save
-    Rails.logger.debug('TRANSCRIPT SAVED') and return if stt_cmd_stderr
   end
 
   private
