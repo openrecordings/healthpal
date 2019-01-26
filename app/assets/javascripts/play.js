403c67459e4d42a605343c0c4366c8c31d7b03fb
Will Haslett <will.haslett@gmail.com>
2019-01-26 16:51:20 -0500

    Started on player JS

---
+++ app/assets/javascripts/play.js
@@ -0,0 +1,17 @@
+function getXPosition {
+}
+
+$(document).ready(function() {
+  let playhead = $('#playhead');
+  let timeline = $('#timeline');
+
+  playhead.draggable({axis: 'x'});
+
+  timeline.click(function(){
+    playhead.
+  })
+
+});
+
+
+
