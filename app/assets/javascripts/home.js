d6382e5b56a1f5c1884239d2a6b43207fc6a0142
Will Haslett <will.haslett@gmail.com>
2019-01-27 21:40:56 -0500

    CSS

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -2,7 +2,7 @@
 // All this logic will automatically be available in application.js.
 
 $(document).ready(function() {
-  $(".tablesorter").each(function(index, value) {
-    $(this).tablesorter({sortList: [[$(this).data("sort-column"), $(this).data("sort-desc")]]});
-  });
+
+    $('.menu').menu();
+
 });
