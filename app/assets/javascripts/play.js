2ef7dc5f2089a19cc8935bc2bf8ad728040e898c
Will Haslett <will.haslett@gmail.com>
2019-02-02 09:24:07 -0500

    Playhead now follows playback time

--- app/assets/javascripts/play.js
+++ app/assets/javascripts/play.js
@@ -8,7 +8,57 @@ if(document.querySelector('#play-pause-button')) {
       let src = 'data:audio/flac;base64,' + data
       $(audioElement).attr('src', src);
     });
+    // Playhead follows audio element during playback
+    audioElement.ontimeupdate = function(event){skipToTime(audioElement.currentTime) }
   }
+
+  // Expects newTime to be a float
+  function skipToTime(newTime){
+    let audioElement = document.getElementById('audio-element');
+    let eventX = event.pageX;
+    let timeline = $('#timeline');
+    let timelinePosition = timeline.position();
+    let playhead = $('#playhead');
+    let currentTime = $(audioElement).prop('currentTime');
+    let duration = $(audioElement).prop('duration');
+    let PxPerSec = $('#timeline').width() / duration;
+    let progressBar = $('#progress-bar');
+    let newPx = PxPerSec * newTime 
+    playhead.css({left: newPx});
+    progressBar.css({width: newPx - 10});
+  }
+
+  function skipToEventPosition(event){
+    let audioElement = document.getElementById('audio-element');
+    let eventX = event.pageX;
+    let timeline = $('#timeline');
+    let timelinePosition = timeline.position();
+    let playhead = $('#playhead');
+    let currentTime = $(audioElement).prop('currentTime');
+    let duration = $(audioElement).prop('duration');
+    let secPerPx = duration / $('#timeline').width();
+    let newTime = secPerPx * eventX;
+    let progressBar = $('#progress-bar');
+    playhead.css({left: eventX});
+    if(timelinePosition.left + timeline.width() >= eventX){
+      progressBar.css({width: timelinePosition.left + eventX - 10});
+    }
+    $(audioElement).prop('currentTime', newTime);
+  }
+
+  // Progress bar click
+  $('#timeline').click(function(event){
+    skipToEventPosition(event);
+  })
+
+  // Playhead drag
+  $('#playhead').draggable({
+    axis: 'x',
+    containment: '#timeline',
+    drag: function(event, ui){
+      skipToEventPosition(event);
+    }
+  });
 
   $('#back-button').click(function(){
     let audioElement = document.getElementById('audio-element');
@@ -27,40 +77,6 @@ if(document.querySelector('#play-pause-button')) {
     }
     $('#play-glyph, #pause-glyph, #play-label, #pause-label').toggleClass('hidden');
   })
-
-  // Handles both timeline clicks and playhead drags.
-  function skipToTime(event){
-    let audioElement = document.getElementById('audio-element');
-    let eventX = event.pageX;
-    let timeline = $('#timeline');
-    let timelinePosition = timeline.position();
-    let playhead = $('#playhead');
-    let currentTime = $(audioElement).prop('currentTime');
-    let duration = $(audioElement).prop('duration');
-    let secPerPx = duration / $('#timeline').width();
-    let newTime = secPerPx * eventX;
-    let progressBar = $('#progress-bar');
-    let progressBarPosition = progressBar.position(); 
-    playhead.css({left: eventX});
-    if(timelinePosition.left + timeline.width() >= eventX){
-      progressBar.css({width: timelinePosition.left + eventX - 10});
-    }
-    $(audioElement).prop('currentTime', newTime);
-  }
-
-  // Progress bar click
-  $('#timeline').click(function(event){
-    skipToTime(event);
-  })
-
-  // Playhead drag
-  $('#playhead').draggable({
-    axis: 'x',
-    containment: '#timeline',
-    drag: function(event, ui){
-      skipToTime(event);
-    }
-  });
 
   // Onload
   /////////////////////////////////////////////////////////////////////////////////////////////////
