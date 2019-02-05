d54cde004535c39c9aeb23c6dd07cf6703143030
Will Haslett <will.haslett@gmail.com>
2019-02-05 14:10:26 -0500

    Copied in old utterance and transcript content

---
+++ app/models/utterance.rb
@@ -0,0 +1,11 @@
+# Holds one utterance (block of uninterrupted speech) from a transcript
+#  index:     position of utterance in transcript (e.g. 2 is the second utterance)
+#  begins_at: the starting poing of the utterance in the recording, in seconds 
+#  text:      the ascii content of the utterance
+class Utterance < ApplicationRecord
+  belongs_to :transcript
+  has_many :tags, dependent: :destroy
+
+  # TODO: Encrypt the text attribute
+  
+end
