8e1fe509796a041483716f5cb312db563fc6e9ac
Will Haslett <will.haslett@gmail.com>
2019-02-05 21:55:12 -0500

    Fixed tag validation

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,5 +3,5 @@ class Tag < ApplicationRecord
   belongs_to :tag_type
 
   validates_presence_of :utterance, :tag_type
-  validates_uniqueness_of :tag_type
+  validates_uniqueness_of :tag_type, scope: :utterance
 end
