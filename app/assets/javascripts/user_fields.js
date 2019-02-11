ade68af9bae168bcfa551614c0242a6fd85422e0
Will Haslett <will.haslett@gmail.com>
2019-02-11 05:50:24 -0500

    Working on UserField JS

--- app/assets/javascripts/user_fields.js
+++ app/assets/javascripts/user_fields.js
@@ -1,4 +1,13 @@
 $('textarea').click(function(event){
   let contentField = event.target;
   let saveLink = $(contentField).parent().find('.save');
+  let cancelLink = $(contentField).parent().find('.cancel');
+  $(saveLink).show();
+  $(cancelLink).show();
 })
+
+$(document).click(function(event){
+  if(!$(event.target).closest('.user-field-content').length){
+    $('.save, .cancel').hide();
+  }
+});
