715ec13d34c4da328bfa0a0e16db73b93c4e5538
Will Haslett <will.haslett@gmail.com>
2019-02-13 12:21:30 -0500

    Filter/search radio buttons in place

--- app/assets/javascripts/user_fields.js
+++ app/assets/javascripts/user_fields.js
@@ -1,62 +1,68 @@
-// User fields
-///////////////////////////////////////////////////////////////////////////////////////////////////
-function hideSaveCancel(){
-  $('.save, .cancel').hide();
-}
+if(document.querySelector('.user-field-content')){
 
-$('.user-field-content').click(function(event){
-  let contentField = event.target;
-  let saveLink = $(contentField).parent().find('.save');
-  let cancelLink = $(contentField).parent().find('.cancel');
-  $(saveLink).show();
-  $(cancelLink).show();
-})
-
-$(document).click(function(event){
-  if(!$(event.target).closest('.user-field-content').length){
-    hideSaveCancel();
+  // User fields
+  ///////////////////////////////////////////////////////////////////////////////////////////////////
+  function hideSaveCancel(){
+    $('.save, .cancel').hide();
   }
-});
 
-$('.save').click(function(event){
-  hideSaveCancel();
-  let userField = $(event.target).closest('.user-field-container').find('.user-field-content:first');
-  let recordingId = $(userField).data('recording-id');
-  let type = $(userField).data('type');
-  let text = $(userField).val();
-	$.post('user_field', {type: type, text: text})
-});
+  $('.user-field-content').click(function(event){
+    let contentField = event.target;
+    let saveLink = $(contentField).parent().find('.save');
+    let cancelLink = $(contentField).parent().find('.cancel');
+    $(saveLink).show();
+    $(cancelLink).show();
+  })
 
-$('.cancel').click(function(event){
-  hideSaveCancel();
-});
-
-// Search
-///////////////////////////////////////////////////////////////////////////////////////////////////
-function handleQuery(query){
-  $('.tag-row').each(function(){
-    let rowText = $(this).data('text');
-    log(query);
-    if(!(rowText.includes(query))){
-      $(this).hide();
-    } else {
-      $(this).show();
+  $(document).click(function(event){
+    if(!$(event.target).closest('.user-field-content').length){
+      hideSaveCancel();
     }
   });
-}
 
-// Alpha-numeric keys
-$('#search-input').keypress(function(event){
-  let inputElement = event.target;
-  let query = $(inputElement).val() + String.fromCharCode(event.which);
-  handleQuery(query);
-})
+  $('.save').click(function(event){
+    hideSaveCancel();
+    let userField = $(event.target).closest('.user-field-container').find('.user-field-content:first');
+    let recordingId = $(userField).data('recording-id');
+    let type = $(userField).data('type');
+    let text = $(userField).val();
+    $.post('user_field', {type: type, text: text})
+  });
 
-// Delete/backspace key
-$('#search-input').keyup(function(event){
-  if(event.which == 8){
-    let inputElement = event.target;
-    let query = $(inputElement).val()
-    handleQuery(query);
+  $('.cancel').click(function(event){
+    hideSaveCancel();
+  });
+
+  // Search
+  ///////////////////////////////////////////////////////////////////////////////////////////////////
+  function handleQuery(query){
+    $('.tag-row').each(function(){
+      let rowText = $(this).data('text');
+      if(!(rowText.includes(query))){
+        $(this).hide();
+      } else {
+        $(this).show();
+      }
+    });
   }
-})
+
+  // Alpha-numeric keys
+  $('#search-input').keypress(function(event){
+    let inputElement = event.target;
+    let query = $(inputElement).val() + String.fromCharCode(event.which);
+    handleQuery(query);
+  })
+
+  // Delete/backspace key
+  $('#search-input').keyup(function(event){
+    if(event.which == 8){
+      let inputElement = event.target;
+      let query = $(inputElement).val()
+      handleQuery(query);
+    }
+  })
+  // Onload
+  /////////////////////////////////////////////////////////////////////////////////////////////////
+  $(document).ready(function() {
+  });
+}
