7049c2bbb33ee9f21e457c908be62a89fd774915
Will Haslett <will.haslett@gmail.com>
2019-02-16 09:44:25 -0500

    Tabs work

--- app/assets/javascripts/tabs.js
+++ app/assets/javascripts/tabs.js
@@ -1,2 +1,12 @@
 if(document.querySelector('#index-view')) {
+  $('.tab').click(function(){
+    let userId = $(this).data('user-id');
+    $('.tab').removeClass('selected');
+    $(this).addClass('selected');
+    $('.content').hide();
+    $(`[data-user-id=${userId}]`).show();
+  })
+
+  $(document).ready(function() {
+  });
 }
