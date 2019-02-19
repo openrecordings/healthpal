e6bce6ca696b7156a35c110da230be87fe6cfb67
Will Haslett <will.haslett@gmail.com>
2019-02-19 10:38:37 -0500

    Set up email config for staging and production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,11 @@ Rails.application.configure do
   # Use NGINX for send_file
   config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
 
+  # Staging and Production:
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.default_url_options = { host: ENV['host'] }
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
