02c8d68dceabc18705e0f38b6a861881096fbeb4
Will Haslett <will.haslett@gmail.com>
2019-04-04 13:23:52 -0400

    Audio level meter is working

--- app/assets/javascripts/record.js
+++ app/assets/javascripts/record.js
@@ -1,5 +1,5 @@
 if(document.querySelector('#record-start-button')) {
-  const videoElement = document.querySelector('video');
+  const audioElement = document.querySelector('audio');
   var streamRecorder;
   var recordStream;
   var mediaRecorder;
@@ -21,7 +21,7 @@ if(document.querySelector('#record-start-button')) {
     console.log('got');
     // Make stream available to console
     window.stream = stream;
-	  videoElement.srcObject = stream;
+	  audioElement.srcObject = stream;
     recordStream = stream;
   }
 
@@ -70,175 +70,32 @@ if(document.querySelector('#record-start-button')) {
     console.log('video uploaded');
   }
 
-  // Audio level measurement starts here
+  // Audio level measurement https://codepen.io/travisholliday/pen/gyaJk
   ////////////////////////////////////////////////////////////////////////////////////////////////
-  // https://github.com/cwilso/volume-meter/blob/master/volume-meter.js
-
-  /*
-  The MIT License (MIT)
-
-  Copyright (c) 2014 Chris Wilson
-
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files (the "Software"), to deal
-  in the Software without restriction, including without limitation the rights
-  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-  copies of the Software, and to permit persons to whom the Software is
-  furnished to do so, subject to the following conditions:
-
-  The above copyright notice and this permission notice shall be included in all
-  copies or substantial portions of the Software.
-
-  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-  SOFTWARE.
-  */
-
-  /*
-
-  Usage:
-  audioNode = createAudioMeter(audioContext,clipLevel,averaging,clipLag);
-
-  audioContext: the AudioContext you're using.
-  clipLevel: the level (0 to 1) that you would consider "clipping".
-     Defaults to 0.98.
-  averaging: how "smoothed" you would like the meter to be over time.
-     Should be between 0 and less than 1.  Defaults to 0.95.
-  clipLag: how long you would like the "clipping" indicator to show
-     after clipping has occured, in milliseconds.  Defaults to 750ms.
-
-  Access the clipping through node.checkClipping(); use node.shutdown to get rid of it.
-  */
-
-  function createAudioMeter(audioContext,clipLevel,averaging,clipLag) {
-    var processor = audioContext.createScriptProcessor(512);
-    processor.onaudioprocess = volumeAudioProcess;
-    processor.clipping = false;
-    processor.lastClip = 0;
-    processor.volume = 0;
-    processor.clipLevel = clipLevel || 0.98;
-    processor.averaging = averaging || 0.95;
-    processor.clipLag = clipLag || 750;
-
-    // this will have no effect, since we don't copy the input to the output,
-    // but works around a current Chrome bug.
-    processor.connect(audioContext.destination);
-
-    processor.checkClipping =
-      function(){
-        if (!this.clipping)
-          return false;
-        if ((this.lastClip + this.clipLag) < window.performance.now())
-          this.clipping = false;
-        return this.clipping;
-      };
-
-    processor.shutdown =
-      function(){
-        this.disconnect();
-        this.onaudioprocess = null;
-      };
-
-    return processor;
-  }
-
-  function volumeAudioProcess( event ) {
-    var buf = event.inputBuffer.getChannelData(0);
-      var bufLength = buf.length;
-    var sum = 0;
-      var x;
-
-    // Do a root-mean-square on the samples: sum up the squares...
-      for (var i=0; i<bufLength; i++) {
-        x = buf[i];
-        if (Math.abs(x)>=this.clipLevel) {
-          this.clipping = true;
-          this.lastClip = window.performance.now();
-        }
-        sum += x * x;
+	function startMeter(){
+    audioContext = new AudioContext();
+    analyser = audioContext.createAnalyser();
+    microphone = audioContext.createMediaStreamSource(recordStream);
+    javascriptNode = audioContext.createScriptProcessor(2048, 1, 1);
+    analyser.smoothingTimeConstant = 0.8;
+    analyser.fftSize = 1024;
+    microphone.connect(analyser);
+    analyser.connect(javascriptNode);
+    javascriptNode.connect(audioContext.destination);
+    canvasContext = document.querySelector('#audio-meter').getContext("2d");
+    javascriptNode.onaudioprocess = function() {
+      var array = new Uint8Array(analyser.frequencyBinCount);
+      analyser.getByteFrequencyData(array);
+      var values = 0;
+      var length = array.length;
+      for (var i = 0; i < length; i++) {
+        values += (array[i] * 1.2);
       }
-
-      // ... then take the square root of the sum.
-      var rms =  Math.sqrt(sum / bufLength);
-
-      // Now smooth this out with the averaging factor applied
-      // to the previous sample - take the max here because we
-      // want "fast attack, slow release."
-      this.volume = Math.max(rms, this.volume*this.averaging);
-  }
-
-  // Audio level display starts here
-  ////////////////////////////////////////////////////////////////////////////////////////////////
-  // https://ourcodeworld.com/articles/read/413/how-to-create-a-volume-meter-measure-the-sound-
-  // level-in-the-browser-with-javascript
-  /*
-  The MIT License (MIT)
-
-  Copyright (c) 2014 Chris Wilson
-
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files (the "Software"), to deal
-  in the Software without restriction, including without limitation the rights
-  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-  copies of the Software, and to permit persons to whom the Software is
-  furnished to do so, subject to the following conditions:
-
-  The above copyright notice and this permission notice shall be included in all
-  copies or substantial portions of the Software.
-
-  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-  SOFTWARE.
-  */
-  if(videoElement.length > 0 && audioSelect.length > 0 && videoSelect.length > 0){
-    var audioContext = null;
-    var meter = null;
-    var canvasContext = null;
-    var WIDTH=500;
-    var HEIGHT=50;
-    var rafID = null;
-
-    function onMicrophoneDenied() {
-        alert('Stream generation failed.');
-    }
-
-    var mediaStreamSource = null;
-
-    function onMicrophoneGranted(stream) {
-        // Create an AudioNode from the stream.
-        mediaStreamSource = audioContext.createMediaStreamSource(stream);
-
-        // Create a new volume meter and connect it.
-        meter = createAudioMeter(audioContext);
-        mediaStreamSource.connect(meter);
-
-        // kick off the visual updating
-        onLevelChange();
-    }
-
-    function onLevelChange( time ) {
-        // clear the background
-        canvasContext.clearRect(0,0,WIDTH,HEIGHT);
-
-        // check if we're currently clipping
-        if (meter.checkClipping())
-            canvasContext.fillStyle = "red";
-        else
-            canvasContext.fillStyle = "green";
-
-        // draw a bar based on the current volume
-        canvasContext.fillRect(0, 0, meter.volume * WIDTH * 1.4, HEIGHT);
-
-        // set up the next visual callback
-        rafID = window.requestAnimationFrame( onLevelChange );
+      var average = values / length;
+      canvasContext.clearRect(0, 0, 50, 200);
+      canvasContext.fillStyle = '#f9d56d';
+      canvasContext.fillRect(0, 200 - average, 50, 200);
+      canvasContext.fillStyle = '#555';
     }
   }
 
@@ -246,10 +103,13 @@ if(document.querySelector('#record-start-button')) {
   //////////////////////////////////////////////////////////////////////////////////////////////////
   $(document).ready(function() {
     if (hasGetUserMedia()) {
+      $('#record-audio').prop('muted', true);
+
       navigator.mediaDevices.enumerateDevices().then(getStream).catch(handleError);
 
       $('#record-start-button').click(function(){
         startRecording();
+				startMeter();
       })
 
       $('#record-stop-button').click(function(){
