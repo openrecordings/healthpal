811f00dc5e3f7aadfabe4617a0baf3bf7ba7ffd3
Will Haslett <will.haslett@gmail.com>
2019-07-18 14:03:52 -0400

    Removed deprecated content from application.rb

--- config/application.rb
+++ config/application.rb
@@ -10,27 +10,6 @@ module Orals
   class Application < Rails::Application
     config.load_defaults '6.0'
     config.time_zone = 'Eastern Time (US & Canada)'
-    config.app_display_name = ENV['APP_DISPLAY_NAME']
-    config.root_email = ENV['ROOT_EMAIL']
-    config.root_password = ENV['ROOT_PASSWORD']
-
     config.active_job.queue_adapter = :sidekiq
-
-    config.twilio_account_sid = ENV['TWILIO_ACCOUNT_SID']
-    config.twilio_auth_token = ENV['TWILIO_AUTH_TOKEN']
-    config.twilio_from_phone_number = ENV['TWILIO_FROM_PHONE_NUMBER']
-
-    config.cloud_provider = ENV['CLOUD_PROVIDER']
-
-		# Google Cloud Platform Config
-    config.gcp_app_credentials = ENV['GOOGLE_APPLICATION_CREDENTIALS']
-    config.gcp_project_name =  ENV['GCP_PROJECT_NAME']
-		config.gcp_bucket_name = ENV['GCP_BUCKET_NAME']
-
-    # AWS config
-    config.aws_media_bucket_name = ENV['AWS_MEDIA_BUCKET_NAME']
-    config.aws_transcript_bucket_name = ENV['AWS_TRANSCRIPT_BUCKET_NAME']
-    config.aws_region = ENV['AWS_REGION']
-    
   end
 end
