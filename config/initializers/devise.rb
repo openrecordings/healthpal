a8946c2f15d362f85148447cb0eb8f622e417fbc
Will Haslett <will.haslett@gmail.com>
2019-08-27 17:10:15 -0400

    Emailing might work now

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -15,7 +15,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'no-reply@orals.dartmouth.edu'
+  config.mailer_sender = 'no-reply@audiohealthpal.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
