e52dd72a567b8f5eaa072763025e6971e608629e
Will Haslett <will.haslett@gmail.com>
2019-09-03 15:25:28 -0400

    Removed unused WebRTC JS reference

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,4 +7,4 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-import adapter from 'webrtc-adapter';
+// import adapter from 'webrtc-adapter';
