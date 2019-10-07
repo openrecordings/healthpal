8226dfe2edad46ff912158bb157a7efe1ecff1ea
Will Haslett <will.haslett@gmail.com>
2019-10-07 16:51:14 -0400

    RSpec / browserstack works

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,29 +1,15 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
+#browserstack
+require 'yaml'
+require 'rspec'
+require 'selenium-webdriver'
+require 'browserstack/local'
+TASK_ID = (ENV['TASK_ID'] || 0).to_i
+
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'development'
 require File.expand_path('../../config/environment', __FILE__)
-# Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
-# Add additional requires below this line. Rails is not loaded until this point!
-
-# Requires supporting ruby files with custom matchers and macros, etc, in
-# spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
-# run as spec files by default. This means that files in spec/support that end
-# in _spec.rb will both be required and run as specs, causing the specs to be
-# run twice. It is recommended that you do not name files matching this glob to
-# end with _spec.rb. You can configure this pattern with the --pattern
-# option on the command line or in ~/.rspec, .rspec or `.rspec-local`.
-#
-# The following line is provided for convenience purposes. It has the downside
-# of increasing the boot-up time by auto-requiring all files in the support
-# directory. Alternatively, in the individual `*_spec.rb` files, manually
-# require only the support files necessary.
-#
-# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
-
-# Checks for pending migrations and applies them before tests are run.
-# If you are not using ActiveRecord, you can remove these lines.
 begin
   ActiveRecord::Migration.maintain_test_schema!
 rescue ActiveRecord::PendingMigrationError => e
@@ -31,31 +17,42 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
   config.use_transactional_fixtures = true
-
-  # RSpec Rails can automatically mix in different behaviours to your tests
-  # based on their file location, for example enabling you to call `get` and
-  # `post` in specs under `spec/controllers`.
-  #
-  # You can disable this behaviour by removing the line below, and instead
-  # explicitly tag your specs with their type, e.g.:
-  #
-  #     RSpec.describe UsersController, :type => :controller do
-  #       # ...
-  #     end
-  #
-  # The different available types are documented in the features, such as in
-  # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
-
-  # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
-  # arbitrary gems may also be filtered via:
-  # config.filter_gems_from_backtrace("gem name")
+
+  #browserstack
+	browserstack_config = Rails.application.credentials.browserstack
+  server = browserstack_config[:server]
+	user = browserstack_config[:user]
+	key = browserstack_config[:key]
+	common_caps = browserstack_config[:common_caps]
+	browser_caps = browserstack_config[:browser_caps]
+  config.around(:example) do |example|
+    @caps = common_caps.merge(browser_caps[TASK_ID])
+    @caps["name"] = ENV['name'] || example.metadata[:name] || example.metadata[:file_path].split('/').last.split('.').first
+    enable_local = @caps["browserstack.local"] && @caps["browserstack.local"].to_s == "true"
+
+    # Code to start browserstack local before start of test
+    if enable_local
+      @bs_local = BrowserStack::Local.new
+      bs_local_args = { "key" => key, "forcelocal" => true }
+      @bs_local.start(bs_local_args)
+      @caps["browserstack.local"] = true
+    end
+
+    @driver = Selenium::WebDriver.for(:remote,
+      :url => "http://#{user}:#{key}@#{server}/wd/hub",
+      :desired_capabilities => @caps)
+
+    begin
+      example.run
+    ensure 
+      @driver.quit
+      # Code to stop browserstack local after end of test
+      @bs_local.stop if enable_local
+    end
+  end
+
 end
