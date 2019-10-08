bfd0b50a3ac8c85cfe7098f765f8ebc8e8a1d07b
Will Haslett <will.haslett@gmail.com>
2019-10-08 14:58:57 -0400

    More record test work

--- spec/single_spec.rb
+++ spec/single_spec.rb
@@ -2,22 +2,26 @@ require 'rails_helper'
 
 RSpec.describe 'Login and record' do
   
-  it 'can log in the test user' do
+  it 'can make a recording' do
     @driver.navigate.to 'https://audiohealthpal.com'
 
+    # Log in
     email_field = @driver.find_element(:id, 'user_email')
     password_field = @driver.find_element(:id, 'user_password')
     submit_field = @driver.find_element(:id, 'login-button')
-
     email_field.send_keys @test_user_email
     password_field.send_keys @test_user_password
     submit_field.click
-    sleep 2
+    sleep 0.5
     expect(@driver.current_url).to eql('https://audiohealthpal.com/my_recordings')
+
+    # Navigate to recording page
+    @driver.find_element(:id, 'nav-new-recording').click
+    sleep 0.5
+    expect(@driver.current_url).to eql('https://audiohealthpal.com/record')
+
+    # Start recording
+    @driver.find_element(:id, 'record-start-button').click
   end
 
-  it 'can navigate to recording page' do
-    @driver.find_element(:href, '/record').click
-    expect(@driver.current_url).to eql('https://audiohealthpal.com/record')
-  end
 end
