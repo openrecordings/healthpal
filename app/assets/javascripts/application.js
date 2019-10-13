41bb64a8dfb42c239d11379ed2f1bcaf05116de2
Will Haslett <will.haslett@gmail.com>
2019-10-13 14:14:43 -0400

    Re-enabled Ahoy but not deleting cookies

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,21 +5,14 @@
 //= require_tree .
 
 $(document).ready(function() {
-
-  // // Event tracking
-  // /////////////////////////////////////////////////////////////////////////////////////////////////
-  // ahoy.trackAll();
-
-  // // Reset Ahoy cookie on login page
-  // /////////////////////////////////////////////////////////////////////////////////////////////////
-  // if(window.location.pathname == '/users/sign_in'){
-  //   ahoy.reset();
-  // };
-  // 
-  // // Track all elements with the 'track' class. Needed for clicks on elements that are not <a> or <button>
-  // $('.track').click(function(){
-  //   ahoy.track('$click', {id: this.id, tag: this.previousSibling.nodeName, page: window.location.pathname, class: this.classList});
-  // })
+  // Event tracking
+  /////////////////////////////////////////////////////////////////////////////////////////////////
+  ahoy.trackAll();
+  
+  // Track all elements with the 'track' class. Needed for clicks on elements that are not <a> or <button>
+  $('.track').click(function(){
+    ahoy.track('$click', {id: this.id, tag: this.previousSibling.nodeName, page: window.location.pathname, class: this.classList});
+  })
 
   // Utility
   /////////////////////////////////////////////////////////////////////////////////////////////////
