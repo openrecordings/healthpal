83f5b6afb38865c075629b6e41cad8c43e61ba95
Will Haslett <will.haslett@gmail.com>
2019-11-13 13:16:23 -0500

    Started factoring out strings for i18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_cache_buster
+  around_action :set_locale
   after_action :track_action
 
   # Used in a before_filter in individual controllers for authorization.
@@ -10,18 +11,23 @@ class ApplicationController < ActionController::Base
     redirect_to root_url unless current_user and current_user.privileged?
   end
 
-	protected
+  protected
 
   # Allow devise_invitable to handle the role parameter when creating users
-	def configure_permitted_parameters
-		devise_parameter_sanitizer.permit(:invite, keys: [:role])
-	end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:invite, keys: [:role])
+  end
 
   def set_cache_buster
     response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
     response.headers["Pragma"] = "no-cache"
     response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
   end
+
+	def set_locale(&action)
+		locale = current_user&.try(:locale) || I18n.default_locale
+		I18n.with_locale(locale, &action)
+	end
 
   # Used in a before_filter in individual controllers for authorization.
   def only_admins
