4b4f9f71d96c7b93853c3cf5e6b67b2b5634cef6
Will Haslett <will.haslett@gmail.com>
2019-11-13 12:12:50 -0500

    Added locale to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_03_165819) do
+ActiveRecord::Schema.define(version: 2019_11_13_171005) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -180,6 +180,7 @@ ActiveRecord::Schema.define(version: 2019_10_03_165819) do
     t.string "phone_token"
     t.boolean "requires_phone_confirmation"
     t.boolean "onboarded", default: false
+    t.string "locale"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["invitation_token"], name: "index_users_on_invitation_token", unique: true
     t.index ["invitations_count"], name: "index_users_on_invitations_count"
