c7cbfccb4cd273943895406e6685489921126c95
Will Haslett <will.haslett@gmail.com>
2019-11-21 13:44:07 -0500

    User seeding now supports n users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,16 @@
-tag_types = ['medication', 'medical_condition', 'tests_and_imaging', 'treatments_and_procedures']
 
-# Create root user
-unless User.where(email: Rails.application.credentials.root_email).first
-  puts 'Creating root user'
-  user = User.new({
-    email: Rails.application.credentials.root_email,
-    password: Rails.application.credentials.root_password,
-    first_name: 'Will',
-    last_name: 'Haslett',
-    phone_number: 1234567890,
-    requires_phone_confirmation: false,
-    role: 'root',
-    onboarded: true
-  })
-  user.save!
+
+# Create seeded users
+def create_user(user_yaml)
+  unless User.where(email: Rails.application.credentials.root_email).first
+    puts "Creating user for #{user_yaml[:email]} with role #{user_yaml[:role]}"
+    user = User.create!(user_yaml)
+  end
 end
+Rails.application.credentials.seeded_users.each{|u| create_user(u)}
 
 # Create tag types
+tag_types = ['medication', 'medical_condition', 'tests_and_imaging', 'treatments_and_procedures']
 tag_types.each { |tag_name|
   unless TagType.where(label: tag_name).first
     puts 'Adding tag type: ' + tag_name
