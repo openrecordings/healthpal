9783f5f595257ceb8096bbb43d8f2230b71a3383
Will Haslett <will.haslett@gmail.com>
2019-12-03 15:13:04 -0500

    Combined tests for performance

--- spec/everything_spec.rb
+++ spec/everything_spec.rb
@@ -1,14 +1,5 @@
 require 'rails_helper'
-
-## This are the test instructions for recording. The comments divide the code into the different
-## user actions that are simulated. Use this code as a cheat sheet for writing tests that test
-## other functionality. The lines that start with "expect" are the actual boolean pass/fail tests
-## if any one of them fails, the whole thing fails. There are many kinds of boolean tests you can
-## use to verify that the code is working correctly. See:
-##   https://relishapp.com/rspec/rspec-expectations/docs/built-in-matchers
-
-RSpec.describe 'Recording' do
-  
+RSpec.describe 'Everything' do
   it 'can make a recording' do
     # TODO: Remove conditional once Selenium microphone access with Edge is working
     if @browser == 'Edge'
@@ -16,7 +7,6 @@ RSpec.describe 'Recording' do
       true
     else
       @driver.navigate.to 'https://audiohealthpal.com'
-
       # Log in
       email_field = @driver.find_element(:id, 'user_email')
       email_field.send_keys @test_user_email
@@ -28,40 +18,32 @@ RSpec.describe 'Recording' do
       submit_field.click
       sleep 1.0
       expect(@driver.current_url).to eql('https://audiohealthpal.com/my_recordings')
-
       # Navigate to recording page
       @driver.find_element(:id, 'nav-new-recording').click
       sleep 0.5
       expect(@driver.current_url).to eql('https://audiohealthpal.com/record')
-
       # Record and upload
       @driver.find_element(:id, 'record-start-button').click
       sleep 0.5
       @driver.find_element(:id, 'record-stop-button').click
       sleep 0.5
       @driver.find_element(:id, 'save-button').click
-
-      # Check every second for 30 seconds to see if the recording uploaded successfully
       start_time = Time.now
+      # Check every second for 30 seconds to see if the recording uploaded successfully
       while Time.now - start_time < 30
         sleep 1
         break if @driver.current_url.include?('https://audiohealthpal.com/my_recordings')
       end
       expect(@driver.current_url).to include('https://audiohealthpal.com/my_recordings')
-      end
+      # Select a recording
+      @driver.find_element(:link, 'Tue, Nov 26, 2019').click
+      sleep 0.5
+      expect(@driver.current_url).to include('https://audiohealthpal.com/play')
+      # Play
+      sleep 4.0
+      @driver.find_element(:id, 'play').click
+      sleep 2.0
+      expect(@driver.find_element(:id, 'video-element').attribute('currentTime').to_i).to be > 10.0
+    end
   end
-  
-  # it 'can play a recording' do
-  #   @driver.navigate.to 'https://audiohealthpal.com'
-  #   # Select a recording
-  #   @driver.find_element(:link, 'Sat, Jan 1, 2000').click
-  #   sleep 0.5
-  #   expect(@driver.current_url).to include('https://audiohealthpal.com/play')
-  #   # Play
-  #   sleep 4.0
-  #   @driver.find_element(:id, 'play').click
-  #   sleep 2.0
-  #   expect(@driver.find_element(:id, 'video-element').attribute('currentTime').to_i).to be > 0.5
-  # end
-
 end
