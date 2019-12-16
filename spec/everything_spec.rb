7476ba43073b705790c3fb8a501b4c5c2305fb28
Will Haslett <will.haslett@gmail.com>
2019-12-16 18:36:09 -0500

    Surgery on Browserstack setup - new YAML files

--- spec/everything_spec.rb
+++ spec/everything_spec.rb
@@ -13,36 +13,36 @@ RSpec.describe 'Everything' do
       sleep 0.5
       password_field = @driver.find_element(:id, 'user_password')
       password_field.send_keys @test_user_password
-      sleep 0.5
       submit_field = @driver.find_element(:id, 'login-button')
+      sleep 2.0
       submit_field.click
-      sleep 1.0
+      sleep 2.0
       expect(@driver.current_url).to eql('https://audiohealthpal.com/my_recordings')
       # Navigate to recording page
       @driver.find_element(:id, 'nav-new-recording').click
-      sleep 0.5
+      sleep 1.5
       expect(@driver.current_url).to eql('https://audiohealthpal.com/record')
       # Record and upload
       @driver.find_element(:id, 'record-start-button').click
-      sleep 0.5
+      sleep 1.5
       @driver.find_element(:id, 'record-stop-button').click
-      sleep 0.5
+      sleep 1.5
       @driver.find_element(:id, 'save-button').click
       start_time = Time.now
       # Check every second for 30 seconds to see if the recording uploaded successfully
       while Time.now - start_time < 30
-        sleep 1
+        sleep 2.0
         break if @driver.current_url.include?('https://audiohealthpal.com/my_recordings')
       end
       expect(@driver.current_url).to include('https://audiohealthpal.com/my_recordings')
       # Select a recording
       @driver.find_element(:link, 'Tue, Nov 26, 2019').click
-      sleep 0.5
+      sleep 1.5
       expect(@driver.current_url).to include('https://audiohealthpal.com/play')
       # Play
-      sleep 4.0
+      sleep 6.0
       @driver.find_element(:id, 'play').click
-      sleep 2.0
+      sleep 4.0
       expect(@driver.find_element(:id, 'video-element').attribute('currentTime').to_i).to be > 0.0
     end
   end
