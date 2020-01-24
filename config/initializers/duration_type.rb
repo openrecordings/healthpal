136af008245555061a8e79e0eeebfd9cbee2a578
Will Haslett <will.haslett@gmail.com>
2020-01-24 17:17:22 -0500

    Added duration type

---
+++ config/initializers/duration_type.rb
@@ -0,0 +1,14 @@
+# https://stackoverflow.com/a/45359178
+class DurationType < ActiveRecord::Type::String
+  def cast(value)
+    return value if value.blank? || value.is_a?(ActiveSupport::Duration)
+
+    ActiveSupport::Duration.parse(value)
+  end
+
+  def serialize(duration)
+    duration ? duration.iso8601 : nil
+  end
+end
+
+ActiveRecord::Type.register(:duration, DurationType)
