664c83be51bfaba98b5c65ce652f34622783e50f
Will Haslett <will.haslett@gmail.com>
2020-02-17 15:12:23 -0500

    Gem version chaos

--- dev-entrypoint.sh
+++ dev-entrypoint.sh
@@ -1,7 +1,2 @@
 bundle exec whenever --update-crontab
-printenv | sed 's/^\(.*\)$/export \1/g' > /root/project_env.sh
-sed -i '/imklog/s/^/#/' /etc/rsyslog.conf
-touch /var/log/rsyslog
-service rsyslog start
-service start crontab
 rails s -b 0.0.0.0
