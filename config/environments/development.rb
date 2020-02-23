f3732701aa46f3e4f719acfb01bb4fd3f670d7bb
Will Haslett <will.haslett@gmail.com>
2020-02-23 15:10:21 -0500

    Took host cred out of environment level

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,7 +25,7 @@ Rails.application.configure do
   # Staging and production:
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = {host: Rails.application.credentials[:staging][:host]}
+  config.action_mailer.default_url_options = {host: Rails.application.credentials[:host]}
   config.action_mailer.raise_delivery_errors = true
 
   # send email in development. Configured for Letter Opener
