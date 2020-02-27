4fd486a49cb3182154f2bd4dd2f28d8d4854f816
Will Haslett <will.haslett@gmail.com>
2020-02-27 11:25:13 -0500

    Fixed email test

--- app/mailers/devise_mailer.rb
+++ app/mailers/devise_mailer.rb
@@ -6,14 +6,6 @@ class DeviseMailer < Devise::Mailer
     end
 
     def reset_password_instructions(record, token, opts={})
-    end
-
-    def unlock_instructions(record, token, opts={})
-      @token = token
-      devise_mail(record, :unlock_instructions, opts)
-    end
-
-    def email_changed(record, opts={})
       recipient = 'will.haslett@dartmouth.edu'
       ses = Aws::SES::Client.new(region: 'us-east-1')
       begin
@@ -45,6 +37,14 @@ class DeviseMailer < Devise::Mailer
       rescue Aws::SES::Errors::ServiceError => error
         puts "Email not sent. Error message: #{error}"
       end
+    end
+
+    def unlock_instructions(record, token, opts={})
+      @token = token
+      devise_mail(record, :unlock_instructions, opts)
+    end
+
+    def email_changed(record, opts={})
     end
 
     def password_change(record, opts={})
