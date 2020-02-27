5e62e5466aa264f2b956b5bd2614a39e5f110b31
Will Haslett <will.haslett@gmail.com>
2020-02-27 10:36:01 -0500

    Testing SES API override of ActionMailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,7 +13,7 @@ Rails.application.configure do
 	}
 
   # Staging and production:
-  config.action_mailer.perform_deliveries = true
+  config.action_mailer.perform_deliveries = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = {host: Rails.application.credentials[:host]}
   config.action_mailer.raise_delivery_errors = true
