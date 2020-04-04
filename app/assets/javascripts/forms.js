79e2db796dac3e52e795c2039c86f8b99d9edc08
Will Haslett <will.haslett@gmail.com>
2020-04-04 11:16:15 -0400

    Dynamic input labels now handle autofocus

--- app/assets/javascripts/forms.js
+++ app/assets/javascripts/forms.js
@@ -1,5 +1,6 @@
 if(document.querySelector('.form-input-container')) {
-  $(document).ready(function() {
+  $(document).ready(function(){
+    $('input[autofocus]').first().prev().css('visibility', 'visible');
 
     $('.form-input').focusin(function() {
       $(this).prev().css('visibility', 'visible');
