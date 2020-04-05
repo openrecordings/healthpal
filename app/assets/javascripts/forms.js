371b6a8d3c644834347255405625673119176862
Will Haslett <will.haslett@gmail.com>
2020-04-05 07:58:47 -0400

    Form input field features in place

--- app/assets/javascripts/forms.js
+++ app/assets/javascripts/forms.js
@@ -1,12 +1,17 @@
 if(document.querySelector('.form-input-container')) {
   $(document).ready(function(){
-    $('input[autofocus]').first().prev().css('visibility', 'visible');
+    $('input[autofocus]').each(function() {
+      $(this).attr('placeholder', '');
+      $(this).prev().css('visibility', 'visible');
+    })
 
     $('.form-input').focusin(function() {
+      $(this).attr('placeholder', '');
       $(this).prev().css('visibility', 'visible');
     })
 
     $('.form-input').focusout(function() {
+      $(this).attr('placeholder', $(this).data('placeholder'));
       $(this).prev().css('visibility', 'hidden');
     })
 
