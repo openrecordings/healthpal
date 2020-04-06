afaf76c96c55c3bdc85b6cb84d8426347443abef
Will Haslett <will.haslett@gmail.com>
2020-04-06 17:56:33 -0400

    Working on hamburger location

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -1,22 +1,22 @@
-if (document.querySelector('#menu-container')) {
+// if (document.querySelector('#menu-container')) {
 
-	function toggleMenuType(mediaQuery) {
-		if (mediaQuery.matches) {
-			$('#mobile-menu-button').show();
-			$('#menu-container').hide();
-			$('#navbar-right').hide();
-		} else {
-			$('#mobile-menu-button').hide();
-			$('#menu-container').show();
-			$('#navbar-right').show();
-		}
-	}
-	var mobileWidth = window.matchMedia("(max-width: 500px)");
-	toggleMenuType(mobileWidth);
-	mobileWidth.addListener(toggleMenuType);
+// 	function toggleMenuType(mediaQuery) {
+// 		if (mediaQuery.matches) {
+// 			$('#mobile-menu-button').show();
+// 			$('#menu-container').hide();
+// 			$('#navbar-right').hide();
+// 		} else {
+// 			$('#mobile-menu-button').hide();
+// 			$('#menu-container').show();
+// 			$('#navbar-right').show();
+// 		}
+// 	}
+// 	var mobileWidth = window.matchMedia("(max-width: 1000px)");
+// 	toggleMenuType(mobileWidth);
+// 	mobileWidth.addListener(toggleMenuType);
 
-	$('#mobile-menu-button').click(function () {
-		$('#menu-container').slideToggle(200);
-	});
+// 	$('#mobile-menu-button').click(function () {
+// 		$('#menu-container').slideToggle(200);
+// 	});
 
-}
+// }
