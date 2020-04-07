88fe9687fa3840aabcd6b1f76ce59c2f35c24a18
Will Haslett <will.haslett@gmail.com>
2020-04-07 11:52:34 -0400

    Working on flex styling for navbar

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -1,28 +1,29 @@
 if (document.querySelector('#menu-container')) {
-	function toggleMenuType(mediaQuery) {
-		if (mediaQuery.matches) {
-			$('#mobile-menu-button').show();
-			$('#menu-container').hide();
-			$('#navbar-right').hide();
-		} else {
-			$('#mobile-menu-button').hide();
-			$('#menu-container').show();
-			$('#navbar-right').show();
-		}
+	var mediaQueries = {
+		'small': window.matchMedia("(max-width: 500px)"),
+		'medium': window.matchMedia("(max-width: 1000px)"),
 	}
 
+	$('#menu-container #long').click(function (event) {
+		$(event.target).slideToggle(100);
+	});
+
 	$(document).ready(function() {
-		var maxSmall = window.matchMedia("(max-width: 500px)");
-		var maxMedium = window.matchMedia("(max-width: 1000px)");
-		maxSmall.addListener(toggleMenuType);
-		maxMedium.addListener(toggleMenuType);
-
-		toggleSmall(maxSm);
-		toggleMedium(maxMedium);
-
-		$('#mobile-menu-button').click(function () {
-			$('#menu-container').slideToggle(100);
-		});
+		mediaQueries.small.addListener(toggleMenuType);
+		mediaQueries.medium.addListener(toggleMenuType);
 	})
 
 }
+
+
+	// function toggleMenuType(mediaQuery) {
+	// 	if (mediaQuery.matches) {
+	// 		$('#mobile-menu-button').show();
+	// 		$('#menu-container').hide();
+	// 		$('#navbar-right').hide();
+	// 	} else {
+	// 		$('#mobile-menu-button').hide();
+	// 		$('#menu-container').show();
+	// 		$('#navbar-right').show();
+	// 	}
+	// }
