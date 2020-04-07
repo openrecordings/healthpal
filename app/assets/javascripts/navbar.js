dbebbb7e460a89d16105d8781ac6fa25493efcd3
Will Haslett <will.haslett@gmail.com>
2020-04-07 08:37:38 -0400

    Factored menu sections into partials

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -1,22 +1,25 @@
-// if (document.querySelector('#menu-container')) {
+if (document.querySelector('#menu-container')) {
+	var maxSmall = window.matchMedia("(max-width: 500px)");
+	var maxMedium = window.matchMedia("(max-width: 1000px)");
+	maxSmall.addListener(toggleMenuType);
+	maxMedium.addListener(toggleMenuType);
 
-// 	function toggleMenuType(mediaQuery) {
-// 		if (mediaQuery.matches) {
-// 			$('#mobile-menu-button').show();
-// 			$('#menu-container').hide();
-// 			$('#navbar-right').hide();
-// 		} else {
-// 			$('#mobile-menu-button').hide();
-// 			$('#menu-container').show();
-// 			$('#navbar-right').show();
-// 		}
-// 	}
-// 	var mobileWidth = window.matchMedia("(max-width: 1000px)");
-// 	toggleMenuType(mobileWidth);
-// 	mobileWidth.addListener(toggleMenuType);
+	function toggleMenuType(mediaQuery) {
+		if (mediaQuery.matches) {
+			$('#mobile-menu-button').show();
+			$('#menu-container').hide();
+			$('#navbar-right').hide();
+		} else {
+			$('#mobile-menu-button').hide();
+			$('#menu-container').show();
+			$('#navbar-right').show();
+		}
+	}
+	toggleSmall(maxSm);
+	toggleMedium(maxMedium);
 
-// 	$('#mobile-menu-button').click(function () {
-// 		$('#menu-container').slideToggle(200);
-// 	});
+	$('#mobile-menu-button').click(function () {
+		$('#menu-container').slideToggle(100);
+	});
 
-// }
+}
