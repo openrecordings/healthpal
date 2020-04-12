5672cf06de020a15fb27833ecef30d0f683e01db
Will Haslett <will.haslett@gmail.com>
2020-04-12 10:03:20 -0400

    Menu mediqueries for regular users in place

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -1,42 +1,46 @@
 if (document.querySelector('#menu-container')) {
-  var mediaQueries = {
-    'small': window.matchMedia("(max-width: 910px)"),
-    'medium': window.matchMedia("(max-width: 1340px)"),
-  }
-
-  function setMenuType(mediaQuery){
-    if(mediaQueries.small.matches){
-      $('#mobile-menu-button').show();
-      $('.menu-item-right').hide();
-      $('.menu-item-left').hide();
-      $('.menu-item-top').show();
-      $('.menu-item-bottom').show();
-    } else if(mediaQueries.medium.matches){
-      $('#mobile-menu-button').show();
-      $('.menu-item-right').hide();
-      $('.menu-item-left').show();
-      $('.menu-item-top').hide();
-      $('.menu-item-bottom').show();
-    } else {
-      $('#mobile-menu-button').hide();
-      $('.menu-item-right').show();
-      $('.menu-item-left').show();
-      $('#long').hide();
-      $('.menu-item-top').hide();
-      $('.menu-item-bottom').hide();
-  	}
-  }
-
-  $('#mobile-menu-button').click(function() {
-    $('#menu-container #long').slideToggle(100);
-  });
-
   $(document).ready(function() {
-  var privileged = $('#nav-container').data('privileged') == true;
-  // console.log($('#nav-container').data('privileged'));
-  console.log(privileged);
-	setMenuType(mediaQueries.small);
-	setMenuType(mediaQueries.medium);
+    function setMenuType(mediaQuery){
+      if(mediaQueries.small.matches){
+        $('#mobile-menu-button').show();
+        $('.menu-item-right').hide();
+        $('.menu-item-left').hide();
+        $('.menu-item-top').show();
+        $('.menu-item-bottom').show();
+      } else if(mediaQueries.medium.matches){
+        $('#mobile-menu-button').show();
+        $('.menu-item-right').hide();
+        $('.menu-item-left').show();
+        $('.menu-item-top').hide();
+        $('.menu-item-bottom').show();
+      } else {
+        $('#mobile-menu-button').hide();
+        $('.menu-item-right').show();
+        $('.menu-item-left').show();
+        $('#long').hide();
+        $('.menu-item-top').hide();
+        $('.menu-item-bottom').hide();
+      }
+    }
+
+    $('#mobile-menu-button').click(function() {
+      $('#menu-container #long').slideToggle(100);
+    });
+
+    var privileged = $('#nav-container').data('privileged') == true;
+    if(privileged){
+      var mediaQueries = {
+        'small': window.matchMedia("(max-width: 910px)"),
+        'medium': window.matchMedia("(max-width: 1340px)"),
+      }
+    } else {
+      var mediaQueries = {
+        'small': window.matchMedia("(max-width: 800px)"),
+        'medium': window.matchMedia("(max-width: 800px)"),
+      }
+    }
+    setMenuType(mediaQueries.small);
+    setMenuType(mediaQueries.medium);
     mediaQueries.small.addListener(setMenuType);
     mediaQueries.medium.addListener(setMenuType);
   })
