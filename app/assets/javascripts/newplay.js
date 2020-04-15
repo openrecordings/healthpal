b3a4b30636b0e6ff01ff4112444728745567053e
Will Haslett <will.haslett@gmail.com>
2020-04-15 11:37:59 -0400

    Working on selection drawer animation

--- app/assets/javascripts/newplay.js
+++ app/assets/javascripts/newplay.js
@@ -1,5 +1,32 @@
 if (document.querySelector('#play-view')) {
-  $(document).ready(function() {
+  var recordingId = null;
+
+  function showSelect(){
+
+  }
+
+  function hideSelect(){
     console.log('foo');
+    $('#search-and-select').animate({
+      width: '50px'
+    })
+  }
+
+  function showPlayback(){
+
+  }
+
+  function hidePlayback(){
+
+  }
+
+  $(document).ready(function() {
+    hideSelect();
+
+    $('.recording-list-item').click(function(){
+      recordingId = $(this).data('recording-id');
+      showPlayback();
+    })
+
   })
 }
