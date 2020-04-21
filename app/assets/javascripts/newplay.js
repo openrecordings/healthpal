7c5eb9861360bf7a27324f7651cad28778043e8d
Will Haslett <will.haslett@gmail.com>
2020-04-21 12:21:56 -0400

    Toggling left/right pane visibility works

--- app/assets/javascripts/newplay.js
+++ app/assets/javascripts/newplay.js
@@ -2,6 +2,10 @@ if (document.querySelector('#play-view')) {
   var recordingId = null;
 
   function showSelect(){
+    $('#right').css('flex-grow', '0');
+    $('#left').css('flex-grow', '1');
+    $('#search-and-select').fadeIn();
+    $('#playback').hide();
 
   }
 
@@ -10,7 +14,6 @@ if (document.querySelector('#play-view')) {
     $('#right').css('flex-grow', '1');
     $('#playback').fadeIn();
     $('#search-and-select').hide();
-    $('#select-collapsed').fadeIn();
   }
 
   $(document).ready(function() {
@@ -20,6 +23,10 @@ if (document.querySelector('#play-view')) {
     $('.recording-list-item').click(function(){
       recordingId = $(this).data('recording-id');
       showPlayback();
+    })
+
+    $('#show-select').click(function(){
+      showSelect();
     })
 
   })
