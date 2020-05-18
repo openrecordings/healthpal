e0516aaf88893cb9671cf60fe4587e7fa98dd337
Will Haslett <>
2020-05-18 17:00:50 -0400

    Added plan comments in play.js

--- app/assets/javascripts/play.js
+++ app/assets/javascripts/play.js
@@ -17,7 +17,14 @@ if (document.querySelector('#play-view')) {
 
   $(document).ready(function() {
 
-    // showPlayback();
+    // - Management of the video element
+    //   - add it via JS if data-recording-id is not null on load
+    //     plan 1: Have Rails generate the HTML
+    //     plan 2: Add the HTML from here (just fetch the media URL via AJAX)
+    //   - create it when a recording is selected if data-recording-id is initially null
+    //   - replace the src when a new recording is selected
+    // - Build the transport
+    // - Add the logic in play.js.old
 
     $('.recording-list-item').click(function(){
       recordingId = $(this).data('recording-id');
