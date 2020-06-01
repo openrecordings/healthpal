cfe34d1966900b285e32c11cf81a7258c488b362
Will Haslett <>
2020-06-01 14:51:50 -0400

    Removed org_id from recordings table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_21_005056) do
+ActiveRecord::Schema.define(version: 2020_06_01_184944) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -145,7 +145,6 @@ ActiveRecord::Schema.define(version: 2020_05_21_005056) do
     t.boolean "is_processed", default: false
     t.bigint "ahoy_visit_id"
     t.datetime "next_appt_at"
-    t.integer "org_id"
   end
 
   create_table "shares", force: :cascade do |t|
