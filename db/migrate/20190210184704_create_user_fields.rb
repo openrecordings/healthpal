a779e74c317cb1dd26d0367c7b827b64aa3814a9
Will Haslett <>
2020-06-10 09:00:05 -0400

    Detroyed UserField model

--- db/migrate/20190210184704_create_user_fields.rb
+++
@@ -1,9 +0,0 @@
-class CreateUserFields < ActiveRecord::Migration[5.2]
-  def change
-    create_table :user_fields do |t|
-      t.integer :user_id
-      t.string :text
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20200610124605_add_time_to_user_field.rb
+++
@@ -1,6 +0,0 @@
-class AddTimeToUserField < ActiveRecord::Migration[6.0]
-  def change
-    add_column :user_fields, :at, :float 
-    rename_column :user_fields, :type, :kind
-  end
-end
