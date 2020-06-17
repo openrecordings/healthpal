8699b5fa1a4b340b46f6c1f055eb60722be60e51
Will Haslett <>
2020-06-17 15:41:32 -0400

    Factored out recording_fetch method

--- app/controllers/recordings_controller.rb
+++ app/controllers/recordings_controller.rb
@@ -7,8 +7,8 @@ class RecordingsController < ApplicationController
 
   # AJAX GET
   def get_metadata
-    recording = Recording.find_by(id: params[:id])
-    if recording && current_user.viewable_recordings.include?(recording)
+    recording = fetch_recording(params[:id])
+    if recording
       render json: {
         url: helpers.url_for(recording.media_file),
         title: recording.title,
@@ -18,46 +18,41 @@ class RecordingsController < ApplicationController
         notes: recording.recording_notes,
         status: 200
       }
-    else
-      render json: {
-        error: 'Current user does not have permission to access that recording',
-        status: 401
-      }
     end
   end
 
   #AJAX POST
   def update_metadata
-    recording = Recording.find_by(id: params[:id])
-    if recording && current_user.viewable_recordings.include?(recording)
+    recording = fetch_recording(params[:id])
+    if recording
       recording.update(
         title: params[:title],
         provider: params[:provider]
       )
       render json: {status: 200}
-    else
-      render json: {
-        error: 'Current user does not have permission to access that recording',
-        status: 401
-      }
     end
   end
 
-  # Ajax endpoint for in-place editing of recordings
-  def update
-    #TODO: Authorization for this recording for non-privileged users
-    recording = Recording.find_by(id: params[:id])
-    if (recording && recording.update(recording_params))
-      head :no_content
-    else
-      render json: recording&.errors, status: :unproccessable_entity
-    end
+  #AJAX GET
+  def get_notes
   end
 
   private
   
   def recording_params
     params.require(:recording).permit(:provider, :patient_identifier, :note)
+  end
+
+  def fetch_recording(id)
+    recording = Recording.find_by(id: params[:id])
+    if recording && current_user.viewable_recordings.include?(recording)
+      return recording
+    else
+      render json: {
+        error: 'Current user does not have permission to access that recording',
+        status: 401
+      } and return nil
+    end
   end
 
 end
