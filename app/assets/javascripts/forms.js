3cefe4f6ada38d0d964d1d2cd89ec79e3dd21b6d
Will Haslett <>
2020-06-17 09:42:00 -0400

    Overlay form design done for now

--- app/assets/javascripts/forms.js
+++ app/assets/javascripts/forms.js
@@ -3,19 +3,22 @@ if(document.querySelector('form')) {
     $('input[autofocus]').each(function() {
       $(this).attr('placeholder', '');
       $(this).prev().css('visibility', 'visible');
-    })
+    });
+
+    $('input:text').filter(function() {return $(this).val() == '';}).each(function(){
+      $(this).prev().css('visibility', 'hidden');
+    });
 
     $('.form-input').focusin(function() {
       $(this).attr('placeholder', '');
       $(this).prev().css('visibility', 'visible');
-    })
+    });
 
     $('.form-input').focusout(function() {
       $(this).attr('placeholder', $(this).data('placeholder'));
       if( !$(this).val() ) {
         $(this).prev().css('visibility', 'hidden');
       }
-    })
-
-  })
+    });
+  });
 }
