7f3e324a421127967f0722d2eb8e5337dfedd994
Lisa Oh <lisaoh@Lisas-MacBook-Pro.local>
2020-07-06 21:57:45 -0700

    removed button for root users to add admins & edited new registration method

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	if(document.querySelector('#new-admin-form, #new-org-form, #new-caregiver-form, #switch-user-form')) {
+	if(document.querySelector('#new-org-form, #new-caregiver-form, #switch-user-form')) {
 		document.body.addEventListener('ajax:success', function(event) {
 			window.location.assign('/');
 		})
