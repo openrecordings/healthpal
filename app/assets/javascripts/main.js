1694ad8ec82473dd19502fbd30ac9769cd529554
Will Haslett <will.haslett@gmail.com>
2020-07-15 14:04:50 -0400

    Looks like help bubbles are done

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,8 +1,30 @@
 $(document).ready(function() {
 
+  $('.help-button').click(function(){
+    let infoPanel = $(this).children('.info-panel');
+    if(infoPanel.css('visibility') == 'hidden'){
+      $(this).css('z-index', '20');
+      $(infoPanel).css('visibility', 'visible');
+      $('.overlay').show();
+    } else {
+      return
+    }
+  });
+
+  $('.close-panel').mousedown(function(){
+    console.log('foo');
+    $('.overlay').hide();
+    $('.overlay').promise().done(function(){
+      console.log('foo');
+      $('.help-button').css('z-index', '1');
+      $('.info-panel').css('visibility', 'hidden');
+    });
+  });
+
+
   $('#dismiss-flash').click(function(){
     $(this).parent().fadeOut();
-  })
+  });
 
   $('.phone-number').mask('(000) 000-0000');
   
