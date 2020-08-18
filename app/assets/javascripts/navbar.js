9abb9b1eae890d77cb85f6a17fdf33491b18b24a
Will Haslett <will.haslett@gmail.com>
2020-08-18 13:19:49 -0400

    Reverted to original two-breakpoint navbar for admins

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -1,39 +1,48 @@
 if (document.querySelector('#menu-container')) {
-  function setMenuType(mediaQuery) {
-    if (mediaQueries.small.matches) {
-      $('#mobile-menu-button').show();
-      $('#menu-left').hide();
-      $('#menu-right').hide();
-      $('#menu-top').show();
-      $('#menu-bottom').show();
-    } else {
-      $('#long').hide();
-      $('#mobile-menu-button').hide();
-      $('#menu-left').show();
-      $('#menu-right').show();
-      $('#menu-top').hide();
-      $('#menu-bottom').hide();
-    }
-  }
-
-  var privileged = $('#nav-container').data('privileged') == true;
-  if (privileged) {
-    var mediaQueries = {
-      'small': window.matchMedia("(max-width: 910px)"),
-    }
-  } else {
-    var mediaQueries = {
-      'small': window.matchMedia("(max-width: 850px)"),
-    }
-  }
-
-  $('#mobile-menu-button').click(function () {
-    $('#menu-container #long').slideToggle(100);
-  });
-
   $(document).ready(function () {
+    function setMenuType(mediaQuery) {
+      if (mediaQueries.small.matches) {
+        $('#mobile-menu-button').show();
+        $('.menu-item-right').hide();
+        $('.menu-item-left').hide();
+        $('.menu-item-top').show();
+        $('.menu-item-bottom').show();
+      } else if (mediaQueries.medium.matches) {
+        $('#mobile-menu-button').show();
+        $('.menu-item-right').hide();
+        $('.menu-item-left').show();
+        $('.menu-item-top').hide();
+        $('.menu-item-bottom').show();
+      } else {
+        $('#mobile-menu-button').hide();
+        $('.menu-item-right').show();
+        $('.menu-item-left').show();
+        $('#long').hide();
+        $('.menu-item-top').hide();
+        $('.menu-item-bottom').hide();
+      }
+    }
+
+    $('#mobile-menu-button').click(function () {
+      $('#menu-container #long').slideToggle(100);
+    });
+
+    var privileged = $('#nav-container').data('privileged') == true;
+    if (privileged) {
+      var mediaQueries = {
+        'small': window.matchMedia("(max-width: 910px)"),
+        'medium': window.matchMedia("(max-width: 1340px)"),
+      }
+    } else {
+      var mediaQueries = {
+        'small': window.matchMedia("(max-width: 850px)"),
+        'medium': window.matchMedia("(max-width: 0px)"),
+      }
+    }
     setMenuType(mediaQueries.small);
-    $('body').show();
+    setMenuType(mediaQueries.medium);
     mediaQueries.small.addListener(setMenuType);
+    mediaQueries.medium.addListener(setMenuType);
+    $('body').show();
   })
 }
