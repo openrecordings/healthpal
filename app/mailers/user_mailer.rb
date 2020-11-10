287294d20c1e5659f1280fafd8fdf47427917dee
Will Haslett <will.haslett@gmail.com>
2020-11-10 09:47:45 -0500

    Reminder emails now come from Org.contact_email_address

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,16 +1,28 @@
 class UserMailer < ApplicationMailer
   def reminder_1
     @recording = params[:message].recording
-    mail(to: @recording.user.email, subject: 'A reminder to check out your recording')
+    mail(
+      from: @recording.user.org.contact_email_address,
+      to: @recording.user.email,
+      subject: 'A reminder to check out your recording'
+    )
   end
 
   def reminder_2
     @recording = params[:message].recording
-    mail(to: @recording.user.email, subject: 'A reminder to check out your recording')
+    mail(
+      from: @recording.user.org.contact_email_address,
+      to: @recording.user.email,
+      subject: 'A reminder to check out your recording'
+    )
   end
 
   def reminder_3
     @recording = params[:message].recording
-    mail(to: @recording.user.email, subject: 'Your next recorded visit is coming up')
+    mail(
+      from: @recording.user.org.contact_email_address,
+      to: @recording.user.email,
+      subject: 'Your next recorded visit is coming up'
+    )
   end
 end
