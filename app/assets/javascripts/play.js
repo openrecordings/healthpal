66723bdfcec860f5832175b7455b79d02a1c9784
Will Haslett <will.haslett@gmail.com>
2020-12-01 10:39:08 -0500

    Translation updates. Placeholders for note controls

--- app/assets/javascripts/play.js
+++ app/assets/javascripts/play.js
@@ -113,7 +113,24 @@ if (document.querySelector('#play-view')) {
       $(window).resize();
     });
 
+    function i18n(locale, string) {
+      return {
+        'en': {
+          'playAt': 'Play at',
+          'editNote': 'Edit note',
+          'deleteNote': 'Delete note',
+        },
+        'es': {
+          'playAt': 'play_at',
+          'editNote': 'edit_note',
+          'deleteNote': 'delete_note',
+        },
+      }[locale][string]
+    }
+
     function noteHtml(note) {
+      var locale = document.documentElement.lang;
+      console.log(locale);
       return `
       <div class='note' data-recording-id=${recordingId} data-note-id=${note.id} data-note-at=${note.at}>
         <div class='note-text'>${note.text}</div>
@@ -122,14 +139,14 @@ if (document.querySelector('#play-view')) {
             <svg width="17" height="17" fill="rgb(54, 125, 119)" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
               <path id="play" d="M24.8175,16.86432,9.503,25.77667A1,1,0,0,1,8,24.91235V7.08765a1,1,0,0,1,1.503-.86432L24.8175,15.13568A1.00006,1.00006,0,0,1,24.8175,16.86432Z"/>
             </svg>
-              <span>Play at ${toMmSs(note.at)}</span>
+              <span>${i18n(locale, 'playAt')} ${toMmSs(note.at)}</span>
           </span>
           <span class='edit-note'>
             <svg width="17" height="17" fill="rgb(54, 125, 119)" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
               <path id="pencil" d="M27.414,8.82812l-2.08588,2.08594L21.08588,6.67188l2.08587-2.08594a.99985.99985,0,0,1,1.41425,0l2.828,2.82812A.99986.99986,0,0,1,27.414,8.82812ZM8.08594,19.67188l4.24218,4.24218L23.91406,12.32812,19.67181,8.08594ZM4.27667,27.09863a.50005.50005,0,0,0,.62476.62476l5.92773-2.48023L6.75677,21.1709Z"/>
             </svg>
             <span>
-              Edit note
+              ${i18n(locale, 'editNote')}
             </span>
           </span>
           <span class='delete-note'>
@@ -137,7 +154,7 @@ if (document.querySelector('#play-view')) {
               <path id="bin" d="M7,26a2.00006,2.00006,0,0,0,2,2H23a2.00006,2.00006,0,0,0,2-2V10H7ZM20,14a1,1,0,0,1,2,0V24a1,1,0,0,1-2,0Zm-5,0a1,1,0,0,1,2,0V24a1,1,0,0,1-2,0Zm-5,0a1,1,0,0,1,2,0V24a1,1,0,0,1-2,0ZM26,6V8H6V6A1,1,0,0,1,7,5h6V4a1,1,0,0,1,1-1h4a1,1,0,0,1,1,1V5h6A1,1,0,0,1,26,6Z"/>
             </svg>
             <span>
-              Delete note
+              ${i18n(locale, 'deleteNote')}
             </span>
           </span>
         </div>
@@ -407,7 +424,7 @@ if (document.querySelector('#play-view')) {
       let form = $('#note-form');
       form.data('note-id', null);
       form.data('note-at', noteAt);
-      $('#note-form-title').text(`New note at ${toMmSs(noteAt)}`);
+      $('#note-time').text(toMmSs(noteAt));
       $('#note-overlay').hide().fadeIn(200);
       $('#note-overlay').css('visibility', 'visible');
     });
