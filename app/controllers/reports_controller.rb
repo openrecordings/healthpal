baceca877bceacf8174d0c009ea22c1bc695b1f4
Will Haslett <will.haslett@gmail.com>
2020-12-21 08:56:51 -0500

    get_records method in place

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,6 +1,21 @@
 class ReportsController < ApplicationController
-
   def dashboard
+    @records = get_records
+  end
+
+  private
+
+  def get_records
+    post_fields = {
+      token: Settings::API_TOKEN,
+      content: 'record',
+      format: 'json',
+      type: 'flat'
+    }
+    Curl::Easy.http_post(
+      Settings::API_URL,
+      fields.collect { |k, v| Curl::PostField.content(k.to_s, v) }
+    ).body_str
   end
 
 end
