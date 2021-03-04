8e1260cdfb27cbf119c432330b6998023352c53a
Will Haslett <will.haslett@gmail.com>
2021-03-03 19:02:07 -0500

    Added send_sms_token to admin-created caregiver accounts

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -126,6 +126,7 @@ class AdminController < ApplicationController
     )
     @user.save
     @user.invite!
+    @user.send_sms_token
     Share.create(
       user_id: params['sharer_id'],
       shared_with_user_id: @user.id
