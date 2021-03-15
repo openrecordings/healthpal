9d70c1e7f81db67c47d7b88de64fbdc32f78729f
Will Haslett <will.haslett@gmail.com>
2021-03-14 22:33:58 -0400

    Accrual charts render

--- app/assets/javascripts/reports.js
+++ app/assets/javascripts/reports.js
@@ -1,89 +1,88 @@
 $(document).ready(function () {
   if ($('#reports-page').length) {
-    console.log('----------Reports JS')
-    // console.log($('#reports-page').data('recruitment')[0]['chart_data']);
-    console.log($('#reports-page').data('recruitment'));
+    var recruitmentData = $('#reports-page').data('recruitment');
+    console.log(recruitmentData);
+    var orgData;
+    var orgName;
+    var chartData;
+    var chartConfig;
+    var color = Chart.helpers.color;
+    var gray = color(window.chartColors.gray).alpha(0.15).rgbString();
+    [1, 2, 3].forEach(function (orgId) {
+      orgData = recruitmentData[orgId - 1];
+      console.log(orgData);
+      orgName = orgData['org_name'];
+      chartData = orgData['chart_data'];
+      chartConfig = {
+        type: 'line',
+        data: {
+          datasets: [
+            {
+              label: 'Enrolled participants',
+              lineTension: 0,
+              backgroundColor: gray,
+              borderColor: gray,
+              fill: true,
+              pointBackgroundColor: gray,
+              pointBorderColor: gray,
+              pointRadius: '4',
+              data: chartData
+            }
+          ]
+        },
+        options: {
+          responsive: true,
+          aspectRatio: 1.5,
+          title: {
+            display: true,
+            text: 'Participant Accrual',
+            fontSize: '22'
+          },
+          legend: {
+            display: false,
+          },
+          scales: {
+            xAxes: [{
+              type: 'time',
+              time: {
+                unit: 'day'
+              },
+              display: true,
+              ticks: {
+                fontSize: '13'
+              }
+            }],
+            yAxes: [{
+              display: true,
+              scaleLabel: {
+                display: true,
+                labelString: 'Total Enrolled',
+                fontSize: '20',
+              },
+              ticks: {
+                max: 30,
+                fontSize: '16'
+              }
 
-  //   $('#enrolled-header').click(function () {
-  //     $('#enrolled-content').toggle();
-  //     $('.enrolled-collapse').toggleClass('hidden');
-  //     $('#enrolled-header').toggleClass('bottom-radii');
-  //   })
-
-  //   $('#screened-header').click(function () {
-  //     $('#screened-content').toggle();
-  //     $('.screened-collapse').toggleClass('hidden');
-  //     $('#screened-header').toggleClass('bottom-radii');
-  //   })
-
-  //   $('#summary-header').click(function () {
-  //     $('#summary-content').toggle();
-  //     $('.summary-collapse').toggleClass('hidden');
-  //     $('#summary-header').toggleClass('bottom-radii');
-  //   })
-
-  //   var chartData = $('#dashboard').data('chart');
-  //   console.log(chartData);
-  //   var color = Chart.helpers.color;
-  //   var gray = color(window.chartColors.gray).alpha(0.15).rgbString();
-  //   var darkGray = color(window.chartColors.gray).alpha(0.5).rgbString();
-  //   var chartConfig = {
-  //     type: 'line',
-  //     data: {
-  //       datasets: [
-  //         {
-  //           label: 'Enrolled participants',
-  //           lineTension: 0,
-  //           backgroundColor: gray,
-  //           borderColor: gray,
-  //           fill: true,
-  //           pointBackgroundColor: gray,
-  //           pointBorderColor: gray,
-  //           pointRadius: '4',
-  //           data: chartData.series1
-  //         }
-  //       ]
-  //     },
-  //     options: {
-  //       responsive: true,
-  //       aspectRatio: 1.5,
-  //       title: {
-  //         display: true,
-  //         text: 'Participant Accrual',
-  //         fontSize: '22'
-  //       },
-  //       legend: {
-  //         display: false,
-  //       },
-  //       scales: {
-  //         xAxes: [{
-  //           type: 'time',
-  //           time: {
-  //             unit: 'day'
-  //           },
-  //           display: true,
-  //           ticks: {
-  //             fontSize: '13'
-  //           }
-  //         }],
-  //         yAxes: [{
-  //           display: true,
-  //           scaleLabel: {
-  //             display: true,
-  //             labelString: 'Total Enrolled',
-  //             fontSize: '20',
-  //           },
-  //           ticks: {
-  //             fontSize: '16'
-  //           }
-
-  //         }]
-  //       }
-  //     }
-  //   };
-  //   var accrualChart = $('#accrual-chart');
-  //   window.accrual = new Chart(accrualChart, chartConfig);
+            }]
+          }
+        }
+      };
+      if (orgId == 1) {
+        window.chart1 = new Chart('chart-1', chartConfig);
+      } else if (orgId == 2){
+        window.chart2 = new Chart('chart-2', chartConfig);
+      } else {
+        window.chart3 = new Chart('chart-3', chartConfig);
+      }
+    })
 
   };
 
 });
+
+    //   $('#enrolled-header').click(function () {
+    //     $('#enrolled-content').toggle();
+    //     $('.enrolled-collapse').toggleClass('hidden');
+    //     $('#enrolled-header').toggleClass('bottom-radii');
+    //   })
