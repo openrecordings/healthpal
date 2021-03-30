0ba8a36b5dcdcc7ea033856a97c7439c086aea52
Will Haslett <will.haslett@gmail.com>
2021-03-30 12:27:54 -0400

    Filtering by annotation concept works

--- app/assets/javascripts/play.js
+++ app/assets/javascripts/play.js
@@ -1,11 +1,11 @@
-if(document.querySelector('#play-pause-button')) {
+if (document.querySelector('#play-pause-button')) {
   var playVolume = 1.0;
   var autoScrollDisabled = false;
 
-  function showPage(){
+  function showPage() {
     $('#loader-container').hide();
     let videoElement = document.getElementById('video-element');
-    if($(videoElement).data('is-video')){
+    if ($(videoElement).data('is-video')) {
       $('#video-play-view').removeClass('invisible');
       $(videoElement).show();
     } else {
@@ -13,42 +13,40 @@ if(document.querySelector('#play-pause-button')) {
     }
   }
 
-  function stripeTable(){
+  function stripeTable() {
     $('.tag-row:visible:odd').addClass('odd-row');
     $('.tag-row:visible:even').addClass('even-row');
   }
 
-  function togglePlayPause(){
+  function togglePlayPause() {
     let videoElement = document.getElementById('video-element');
     if (videoElement.paused) {
-       videoElement.play();
-    }   
+      videoElement.play();
+    }
     else {
-       videoElement.pause();
+      videoElement.pause();
     }
     $('#play-glyph, #pause-glyph, #play-label, #pause-label').toggleClass('invisible');
   }
 
   // Playback control
   /////////////////////////////////////////////////////////////////////////////////////////////////
-  function loadAudio(){
+  function loadAudio() {
     let videoElement = document.getElementById('video-element');
     let recordingId = $(videoElement).data('recording-id');
     videoElement.volume = playVolume;
     skipToTime(0);
   }
 
-  $.arrayIntersect = function(a, b)
-  {
-    return $.grep(a, function(i)
-    {
+  $.arrayIntersect = function (a, b) {
+    return $.grep(a, function (i) {
       return $.inArray(i, b) > -1;
     });
   };
 
-  function playerListener(){
+  function playerListener() {
     let videoElement = document.getElementById('video-element');
-    videoElement.ontimeupdate = function(){
+    videoElement.ontimeupdate = function () {
       let currentTime = videoElement.currentTime;
       $('#current-time').text(toMmSs(currentTime));
       skipToTime(currentTime, false);
@@ -57,28 +55,28 @@ if(document.querySelector('#play-pause-button')) {
     };
   }
 
-  function skipToTime(newTime, updatePlayer = true){
+  function skipToTime(newTime, updatePlayer = true) {
     let videoElement = document.getElementById('video-element');
     let timeline = $('#timeline');
     let playhead = $('#playhead');
     let progressBar = $('#progress-bar');
     let duration = $(videoElement).prop('duration');
-    if(newTime < 0){newTime = 0 };
-    if(newTime > duration){newTime = duration};
-    if(updatePlayer){
+    if (newTime < 0) { newTime = 0 };
+    if (newTime > duration) { newTime = duration };
+    if (updatePlayer) {
       // Update is driven by UI event - move player time
       videoElement.currentTime = newTime.toString();
     } else {
       // Update is driven by current play time - move playhead
       let timelineWidth = $('#timeline').width()
       let PxPerSec = timelineWidth / duration;
-      let newPx = PxPerSec * newTime 
-      playhead.css({left: newPx});
-      progressBar.css({width: newPx + 10});
+      let newPx = PxPerSec * newTime
+      playhead.css({ left: newPx });
+      progressBar.css({ width: newPx + 10 });
     }
   }
 
-  function skipToEventPosition(event){
+  function skipToEventPosition(event) {
     let videoElement = document.getElementById('video-element');
     let eventX = event.pageX - 50;
     let timeline = $('#timeline');
@@ -89,49 +87,69 @@ if(document.querySelector('#play-pause-button')) {
     let secPerPx = duration / $('#timeline').width();
     let newTime = secPerPx * eventX;
     let progressBar = $('#progress-bar');
-    playhead.css({left: eventX});
-    if(timelinePosition.left + timeline.width() >= eventX){
-      progressBar.css({width: timelinePosition.left + eventX - 10});
+    playhead.css({ left: eventX });
+    if (timelinePosition.left + timeline.width() >= eventX) {
+      progressBar.css({ width: timelinePosition.left + eventX - 10 });
     }
     videoElement.currentTime = newTime.toString();
   }
 
   // Search and filtering
   ///////////////////////////////////////////////////////////////////////////////////////////////////
-  function updateVisibleRows(){
+  function updateVisibleRows() {
     let queryString = $('#search-input').val() && $('#search-input').val().toLowerCase();
     let tagTypeIdsShown = [];
-    $('.filter-button').each(function(){
-      if($(this).hasClass('filter-on')){
-        tagTypeIdsShown.push($(this).data('tag-type-id') )
+    let conceptsShown = [];
+    $('.filter-button').each(function () {
+      if ($(this).hasClass('filter-on')) {
+        tagTypeIdsShown.push($(this).data('tag-type-id'))
       }
-    })
+    });
+    $('.concept-filter').each(function () {
+      if ($(this).hasClass('concept-filter-on')) {
+        conceptsShown.push($(this).data('concept-text'));
+      }
+    });
     // Test all tag rows against filters and query string
-    $('.tag-row').each(function(){
+    $('.tag-row').each(function () {
+      console.log($(this).data('start-time'));
       let rowTagTypeIds = $(this).data('tag-type-ids');
-      if(!tagTypeIdsShown.length || $.arrayIntersect(rowTagTypeIds, tagTypeIdsShown).length){
-        // Filter check passed
-        let rowText = $(this).data('text').toLowerCase();
-        if(rowText.includes(queryString) || !queryString){
-          // Search string check passed
-          $(this).show();
+      if (!tagTypeIdsShown.length || $.arrayIntersect(rowTagTypeIds, tagTypeIdsShown).length) {
+        // Category check passed
+        console.log('category check passed');
+        let rowConcepts = $(this).data('annotation-concepts');
+        if (!conceptsShown.length || $.arrayIntersect(rowConcepts, conceptsShown).length) {
+          // Concept check passed
+          console.log('concept check passed');
+          let rowText = $(this).data('text').toLowerCase();
+          if (rowText.includes(queryString) || !queryString) {
+            // Search string check passed
+            console.log('search text passed');
+            $(this).show();
+          } else {
+            // Search string check did not pass
+            console.log('search text did not pass');
+            $(this).hide();
+          }
         } else {
-          // Search string check did not pass
+          // Concept check did not pass
+          console.log('concept check did not pass');
           $(this).hide();
         }
       } else {
-        // Filter check did not pass
+        // Category check did not pass
+        console.log('category check did not pass');
         $(this).hide();
       }
     })
-    if($('.tag-row').not(':hidden').length){
+    if ($('.tag-row').not(':hidden').length) {
       $('#empty-result-message').hide();
     } else {
       $('#empty-result-message').show();
     }
   }
 
-  $('.expand-svg').click(function(){
+  $('.expand-svg').click(function () {
     let conceptsContainer = $(this).closest('.filter').find('.concepts-container');
     if ($(this).hasClass('rotate-z-90')) {
       $(this).removeClass('rotate-z-90');
@@ -144,55 +162,55 @@ if(document.querySelector('#play-pause-button')) {
 
   // Time display, row highlighting
   /////////////////////////////////////////////////////////////////////////////////////////////////
-  function toMmSs(seconds){
+  function toMmSs(seconds) {
     let mm = Math.floor(seconds / 60);
     let ss = parseInt(seconds - mm * 60);
-    return `${mm.toString().padStart(2,'0')}:${ss.toString().padStart(2,'0')}`
+    return `${mm.toString().padStart(2, '0')}:${ss.toString().padStart(2, '0')}`
   }
 
   function scrollTable() {
     let highlightedCells = document.querySelector('.highlighted-row')
-    if(highlightedCells && !autoScrollDisabled){
-        highlightedCells.scrollIntoView({
+    if (highlightedCells && !autoScrollDisabled) {
+      highlightedCells.scrollIntoView({
         behavior: 'smooth',
         block: 'center'
       });
     }
   }
 
-  function updateTableHighlighting(currentTime){
+  function updateTableHighlighting(currentTime) {
     $('.tag-row').children().removeClass('highlighted-row');
-    let highlightRow = $('.tag-row').filter(function(){
+    let highlightRow = $('.tag-row').filter(function () {
       return $(this).data('start-time') <= currentTime && $(this).data('end-time') >= currentTime
     })
     highlightRow.children().addClass('highlighted-row');
   }
-  
+
   // Tag Table listeners
   /////////////////////////////////////////////////////////////////////////////////////////////////
-  $('.click-to-seek').click(function(){
+  $('.click-to-seek').click(function () {
     let videoElement = document.getElementById('video-element');
     skipToTime($(this).data('start-time'))
-    if(videoElement.paused){
+    if (videoElement.paused) {
       $('#play-glyph, #pause-glyph, #play-label, #pause-label').toggleClass('invisible');
     }
     videoElement.play();
   })
 
-  $('#tag-table').mouseover(function(){
+  $('#tag-table').mouseover(function () {
     autoScrollDisabled = true;
   })
 
-  $('#tag-table').mouseout(function(){
+  $('#tag-table').mouseout(function () {
     autoScrollDisabled = false;
   })
 
-  $('.external-link').mouseover(function(){
+  $('.external-link').mouseover(function () {
     let row = $(this).closest('.tag-row');
     row.removeClass('tag-row-hover');
   })
 
-  $('.external-link').mouseout(function(){
+  $('.external-link').mouseout(function () {
     let row = $(this).closest('.tag-row');
     row.addClass('tag-row-hover');
   })
@@ -200,59 +218,59 @@ if(document.querySelector('#play-pause-button')) {
 
   // Playback control button listeners
   /////////////////////////////////////////////////////////////////////////////////////////////////
-  $('#timeline').click(function(event){
+  $('#timeline').click(function (event) {
     skipToEventPosition(event);
   })
 
   $('#playhead').draggable({
     axis: 'x',
     eontainment: '#timeline',
-    drag: function(event, ui){
+    drag: function (event, ui) {
       skipToEventPosition(event);
     }
   });
 
-  $('#rewind-button').click(function(){
+  $('#rewind-button').click(function () {
     let videoElement = document.getElementById('video-element');
     skipToTime(0);
   })
 
-  $('#back-button').click(function(){
+  $('#back-button').click(function () {
     let videoElement = document.getElementById('video-element');
     skipToTime(videoElement.currentTime - 10);
   })
 
-  $('#play-pause-button').click(function(){
+  $('#play-pause-button').click(function () {
     togglePlayPause();
   })
 
-  $('#forward-button').click(function(){
+  $('#forward-button').click(function () {
     let videoElement = document.getElementById('video-element');
     skipToTime(videoElement.currentTime + 10);
   })
 
-  $('#mute-button').click(function(){
+  $('#mute-button').click(function () {
     let videoElement = document.getElementById('video-element');
     if (videoElement.volume > 0) {
-       videoElement.volume = 0;
-    }   
+      videoElement.volume = 0;
+    }
     else {
-       videoElement.volume = playVolume;
+      videoElement.volume = playVolume;
     }
     $('#mute-glyph, #unmute-glyph, #mute-label, #unmute-label').toggleClass('hidden');
   })
 
-  $('.external-link').click(function(event) {
+  $('.external-link').click(function (event) {
     event.stopPropagation();
   })
 
   // Sidebar listeners
   /////////////////////////////////////////////////////////////////////////////////////////////////
-  $('#search-input').keyup(function(event){
+  $('#search-input').keyup(function (event) {
     updateVisibleRows();
   })
 
-  $('.concept-filter').click(function(){
+  $('.concept-filter').click(function () {
     let categoryButton = $(this).closest('.filter').find('.filter-button');
     let categoryGlyph = $(this).closest('.filter').find('.filter-check').find('.check-glyph');
     let siblingConceptFilters = $(this).closest('.filter').find('.concept-filter').not($(this));
@@ -265,8 +283,8 @@ if(document.querySelector('#play-pause-button')) {
       if (!siblingConceptFilters.hasClass('concept-filter-on')) {
         // Turn off filter for entire category
         categoryButton.trigger('click');
-      } 
-    // Turning on filter for concept
+      }
+      // Turning on filter for concept
     } else {
       $(this).addClass('concept-filter-on');
       conceptGlyph.removeClass('hidden');
@@ -275,12 +293,12 @@ if(document.querySelector('#play-pause-button')) {
         // Turn on filter for category, but no other concepts
         categoryButton.addClass('filter-on');
         categoryGlyph.removeClass('hidden');
-      } 
+      }
     }
     updateVisibleRows();
   })
 
-  $('.filter-button').click(function(){
+  $('.filter-button').click(function () {
     let categoryGlyph = $(this).find('.check-glyph');
     let conceptGlyphs = $(this).closest('.filter').find('.concept-glyph');
     let conceptFilters = $(this).closest('.filter').find('.concept-filter');
@@ -298,7 +316,7 @@ if(document.querySelector('#play-pause-button')) {
     updateVisibleRows();
   })
 
-  $('#show-all-topics').click(function(){
+  $('#show-all-topics').click(function () {
     $('.tag-row').show();
     $('.check-glyph').addClass('hidden');
     $('#search-input').val('');
@@ -306,22 +324,22 @@ if(document.querySelector('#play-pause-button')) {
 
   // Onload
   /////////////////////////////////////////////////////////////////////////////////////////////////
-  $(document).ready(function() {
+  $(document).ready(function () {
     loadAudio();
     stripeTable();
     playerListener();
 
     let videoElement = document.getElementById('video-element');
 
-    videoElement.oncanplay = function(){
+    videoElement.oncanplay = function () {
       showPage();
     }
 
-    videoElement.ondurationchange = function(){
+    videoElement.ondurationchange = function () {
       $('#duration').text(toMmSs(videoElement.duration));
     }
 
-    videoElement.onended = function(){
+    videoElement.onended = function () {
       $('#play-glyph, #pause-glyph, #play-label, #pause-label').toggleClass('invisible');
     }
 
