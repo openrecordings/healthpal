a7264667ff7b3518d16f71656ec66ca905122beb
Will Haslett <will.haslett@gmail.com>
2021-06-01 14:39:38 -0400

    Updated gems for SES and security patches

--- app/jobs/process_recording_job.rb
+++ app/jobs/process_recording_job.rb
@@ -5,6 +5,5 @@ class ProcessRecordingJob < ApplicationJob
     recording.reload.transcribe
     recording.reload.annotate
     recording.reload.update is_processed: true
-    recording.create_ready_email
   end
 end
