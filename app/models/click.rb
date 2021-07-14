586c5d189b1650a88cd17e077773e178b6c95c81
Will Haslett <will.haslett@gmail.com>
2021-07-14 08:01:47 -0600

    Minor changes to Click documentation

--- app/models/click.rb
+++ app/models/click.rb
@@ -1,8 +1,8 @@
-# Reprents a user press/click on an affordance in the app
+# Represents a [User] press/click on an affordance in the app
 #
 # These are logged in two different ways, as HTTP requests that are captured in [ApplicationController],
-# and as AJAX posts from certain elements for which we are logging clicks on the client
-# Both user and recording are optional, depending on the context of the click
+# and as AJAX POSTs from certain elements for which we are logging clicks on the client
+# Both [User] and [Recording] are optional, depending on the context of the click
 class Click < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to :recording, optional: true
